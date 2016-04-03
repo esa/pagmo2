39f4be6094f0c428525ebe5bdc416d784f03f7ab
Dario Izzo <dario.izzo@gmail.com>
2016-04-03 17:14:01 +0200

    first implementation

--- include/rng.hpp
+++ include/rng.hpp
@@ -8,26 +8,47 @@ namespace pagmo
 {
 
 namespace details {
-	using random_engine_type = std::mt19937_64;
+	using random_engine_type = std::mt19937;
 
 	template <typename dummy>
 	struct random_device_statics
 	{
 		static random_engine_type m_e;
+		static  std::mutex  m_mutex;
 	};
 
 	template<typename dummy>
 	random_engine_type random_device_statics<dummy>::m_e{std::random_device{}()};
+
+	template<typename dummy>
+	std::mutex random_device_statics<dummy>::m_mutex{std::mutex{}};
 }
+
+/// This intends to be a thread-safe substitute for std::random_device, allowing for precise
+/// global seed control in PaGMO. All classes that contain a random engine (thus that generate
+/// random numbers from variates), by default should contain something like:
+//#include "rng.hpp"
+
+///class class_using_random {
+///    explicit class_using_random(args ...... , unsigned int seed = pagmo::random_device{}()) : m_e(seed), m_seed(seed);
+///private:
+///    // Random engine
+///    pagmo_random_engine                m_e;
+///
+///    unsigned int 					m_seedl
+///}
+
 
 class random_device : public details::random_device_statics<void>
 {
 public:
 	unsigned int operator()() {
-		return m_e();
+		std::lock_guard<std::mutex> lock(m_mutex);
+		return static_cast<unsigned int>(m_e());
 	}
 	static void set_seed(unsigned int seed) {
-		m_e.seed(seed);
+		std::lock_guard<std::mutex> lock(m_mutex);
+		m_e.seed(static_cast<random_engine_type::result_type>(seed));
 	}
 
 };
