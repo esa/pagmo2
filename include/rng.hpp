2173fd7166ad175cde98f803de57987f3b4eb3f6
Dario Izzo <dario.izzo@gmail.com>
2016-04-03 17:32:22 +0200

    coding style

--- include/rng.hpp
+++ include/rng.hpp
@@ -6,23 +6,24 @@
 
 namespace pagmo
 {
-
 namespace details {
-	using random_engine_type = std::mt19937;
 
-	template <typename dummy>
-	struct random_device_statics
-	{
-		static random_engine_type m_e;
-		static  std::mutex  m_mutex;
-	};
+using random_engine_type = std::mt19937;
 
-	template<typename dummy>
-	random_engine_type random_device_statics<dummy>::m_e{std::random_device{}()};
+template <typename dummy>
+struct random_device_statics
+{
+    static random_engine_type m_e;
+    static  std::mutex  m_mutex;
+};
 
-	template<typename dummy>
-	std::mutex random_device_statics<dummy>::m_mutex{std::mutex{}};
-}
+template<typename dummy>
+random_engine_type random_device_statics<dummy>::m_e{std::random_device{}()};
+
+template<typename dummy>
+std::mutex random_device_statics<dummy>::m_mutex{std::mutex{}};
+
+} // end namespace details
 
 /// This intends to be a thread-safe substitute for std::random_device, allowing for precise
 /// global seed control in PaGMO. All classes that contain a random engine (thus that generate
@@ -35,22 +36,24 @@ namespace details {
 ///    // Random engine
 ///    random_engine_type               m_e;
 ///
-///    unsigned int 					m_seed;
+///    unsigned int                     m_seed;
 ///}
 
 
 class random_device : public details::random_device_statics<void>
 {
 public:
-	static unsigned int next() {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		return static_cast<unsigned int>(m_e());
-	}
-	static void set_seed(unsigned int seed) {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		m_e.seed(static_cast<random_engine_type::result_type>(seed));
-	}
+    static unsigned int next() 
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return static_cast<unsigned int>(m_e());
+    }
+    static void set_seed(unsigned int seed) 
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_e.seed(static_cast<random_engine_type::result_type>(seed));
+    }
 
 };
 
-} // namespace
+} // end namespace pagmo
