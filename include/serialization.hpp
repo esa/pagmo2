e2d9387ab1403353fd3e171c3775cca5e55057cf
Francesco Biscani <bluescarni@gmail.com>
2016-04-07 16:53:41 +0200

    Include cereal statically.

--- include/serialization.hpp
+++ include/serialization.hpp
@@ -1,10 +1,10 @@
 #ifndef PAGMO_SERIALIZATION_HPP
 #define PAGMO_SERIALIZATION_HPP
 
-#include "external/cereal/include/cereal/archives/binary.hpp"
-#include "external/cereal/include/cereal/archives/json.hpp"
-#include "external/cereal/include/cereal/archives/portable_binary.hpp"
-#include "external/cereal/include/cereal/types/memory.hpp"
-#include "external/cereal/include/cereal/types/polymorphic.hpp"
+#include "external/cereal/archives/binary.hpp"
+#include "external/cereal/archives/json.hpp"
+#include "external/cereal/archives/portable_binary.hpp"
+#include "external/cereal/types/memory.hpp"
+#include "external/cereal/types/polymorphic.hpp"
 
 #endif
