308d60b55d5d2c661f5c769bbf77976e999d404a
Francesco Biscani <bluescarni@gmail.com>
2016-04-15 22:23:59 +0200

    problem: serialization testing.

--- tests/problem.cpp
+++ tests/problem.cpp
@@ -2,12 +2,15 @@
 
 #define BOOST_TEST_MODULE pagmo_problem_test
 #include <boost/test/unit_test.hpp>
-#include <boost/lexical_cast.hpp>
 
+#include <boost/lexical_cast.hpp>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 #include <utility>
 #include <vector>
 
+#include "../include/serialization.hpp"
 #include "../include/types.hpp"
 
 using namespace pagmo;
@@ -17,13 +20,13 @@ struct base_p
 {
 
     base_p(
-        unsigned int nobj = 1, 
-        unsigned int nec = 0, 
-        unsigned int nic = 0, 
-        const vector_double &ret_fit = {1},         
-        const vector_double &lb = {0}, 
+        unsigned int nobj = 1,
+        unsigned int nec = 0,
+        unsigned int nic = 0,
+        const vector_double &ret_fit = {1},
+        const vector_double &lb = {0},
         const vector_double &ub = {1}
-    ) : m_nobj(nobj), m_nec(nec), m_nic(nic), 
+    ) : m_nobj(nobj), m_nec(nec), m_nic(nic),
         m_ret_fit(ret_fit), m_lb(lb), m_ub(ub) {}
 
     vector_double fitness(const vector_double &) const
@@ -56,6 +59,12 @@ struct base_p
         return "Nothing to report";
     }
 
+    template <typename Archive>
+    void serialize(Archive &ar)
+    {
+        ar(m_nobj,m_nec,m_nic,m_ret_fit,m_lb,m_ub);
+    }
+
     unsigned int m_nobj;
     unsigned int m_nec;
     unsigned int m_nic;
@@ -69,11 +78,11 @@ struct base_p
 struct grad_p : base_p
 {
     grad_p(
-        unsigned int nobj = 1, 
-        unsigned int nec = 0, 
-        unsigned int nic = 0, 
-        const vector_double &ret_fit = {1}, 
-        const vector_double &lb = {0}, 
+        unsigned int nobj = 1,
+        unsigned int nec = 0,
+        unsigned int nic = 0,
+        const vector_double &ret_fit = {1},
+        const vector_double &lb = {0},
         const vector_double &ub = {1},
         const vector_double &g = {1},
         const sparsity_pattern &gs = {{0,0}}
@@ -89,20 +98,28 @@ struct grad_p : base_p
         return m_gs;
     }
 
+    template <typename Archive>
+    void serialize(Archive &ar)
+    {
+        ar(cereal::base_class<base_p>(this),m_g,m_gs);
+    }
+
     vector_double m_g;
     sparsity_pattern m_gs;
 };
+
+PAGMO_REGISTER_PROBLEM(grad_p)
 
 // Generates a dummy problem with arbitrary dimensions and return values
 // having the hessians implemented
 struct hess_p : base_p
 {
     hess_p(
-        unsigned int nobj = 1, 
-        unsigned int nec = 0, 
-        unsigned int nic = 0, 
-        const vector_double &ret_fit = {1}, 
-        const vector_double &lb = {0}, 
+        unsigned int nobj = 1,
+        unsigned int nec = 0,
+        unsigned int nic = 0,
+        const vector_double &ret_fit = {1},
+        const vector_double &lb = {0},
         const vector_double &ub = {1},
         const std::vector<vector_double> &h = {{1}},
         const std::vector<sparsity_pattern> &hs = {{{0,0}}}
@@ -118,20 +135,28 @@ struct hess_p : base_p
         return m_hs;
     }
 
+    template <typename Archive>
+    void serialize(Archive &ar)
+    {
+        ar(cereal::base_class<base_p>(this),m_h,m_hs);
+    }
+
     std::vector<vector_double> m_h;
     std::vector<sparsity_pattern> m_hs;
 };
 
+PAGMO_REGISTER_PROBLEM(hess_p)
+
 // Generates a dummy problem with arbitrary dimensions and return values
 // having the hessians and the gradients implemented
-struct full_p : grad_p 
+struct full_p : grad_p
 {
     full_p(
-        unsigned int nobj = 1, 
-        unsigned int nec = 0, 
-        unsigned int nic = 0, 
-        const vector_double &ret_fit = {1}, 
-        const vector_double &lb = {0}, 
+        unsigned int nobj = 1,
+        unsigned int nec = 0,
+        unsigned int nic = 0,
+        const vector_double &ret_fit = {1},
+        const vector_double &lb = {0},
         const vector_double &ub = {1},
         const vector_double &g = {1},
         const sparsity_pattern &gs = {{0,0}},
@@ -149,9 +174,17 @@ struct full_p : grad_p
         return m_hs;
     }
 
+    template <typename Archive>
+    void serialize(Archive &ar)
+    {
+        ar(cereal::base_class<grad_p>(this),m_h,m_hs);
+    }
+
     std::vector<vector_double> m_h;
     std::vector<sparsity_pattern> m_hs;
 };
+
+PAGMO_REGISTER_PROBLEM(full_p)
 
 struct empty
 {
@@ -210,7 +243,7 @@ BOOST_AUTO_TEST_CASE(problem_construction_test)
     BOOST_CHECK_THROW(problem{base_p(1,0,0,fit_2,lb_2,ub_3)}, std::invalid_argument);
     // 4 - gradient sparsity has index out of bounds
     BOOST_CHECK_THROW(problem{grad_p(1,0,0,fit_2,lb_2,ub_2,grad_2, grads_2_outofbounds)}, std::invalid_argument);
-    // 5 - gradient sparsity has a repeating pair 
+    // 5 - gradient sparsity has a repeating pair
     BOOST_CHECK_THROW(problem{grad_p(1,0,0,fit_2,lb_2,ub_2,grad_2, grads_2_repeats)}, std::invalid_argument);
     // 6 - hessian sparsity has index out of bounds
     BOOST_CHECK_THROW(problem{hess_p(1,1,0,fit_2,lb_2,ub_2,hess_22, hesss_22_outofbounds)}, std::invalid_argument);
@@ -263,7 +296,7 @@ BOOST_AUTO_TEST_CASE(problem_construction_test)
         // 2 - We check that the two outputs of human_readable are identical
         BOOST_CHECK(p1_string==p2_string);
         // 3 - We check that the decision vector dimension is copied
-        BOOST_CHECK(p2.get_n() == p1.get_n());     
+        BOOST_CHECK(p2.get_n() == p1.get_n());
     }
 
     // We check the copy constructor
@@ -293,7 +326,7 @@ BOOST_AUTO_TEST_CASE(problem_construction_test)
         BOOST_CHECK(p2.get_gs_dim() == p1.get_gs_dim());
         BOOST_CHECK(p2.get_hs_dim() == p1.get_hs_dim());
         // 4 - We check that the decision vector dimension is copied
-        BOOST_CHECK(p2.get_n() == p1.get_n());            
+        BOOST_CHECK(p2.get_n() == p1.get_n());
     }
 }
 
@@ -330,7 +363,7 @@ BOOST_AUTO_TEST_CASE(problem_assignment_test)
         // 2 - We check that the two outputs of human_readable are identical
         BOOST_CHECK(p1_string==p2_string);
         // 3 - We check that the decision vector dimension is copied
-        BOOST_CHECK(p2.get_n() == p1.get_n());     
+        BOOST_CHECK(p2.get_n() == p1.get_n());
     }
 
     // We check the copy assignment
@@ -361,7 +394,7 @@ BOOST_AUTO_TEST_CASE(problem_assignment_test)
         BOOST_CHECK(p2.get_gs_dim() == p1.get_gs_dim());
         BOOST_CHECK(p2.get_hs_dim() == p1.get_hs_dim());
         // 4 - We check that the decision vector dimension is copied
-        BOOST_CHECK(p2.get_n() == p1.get_n());            
+        BOOST_CHECK(p2.get_n() == p1.get_n());
     }
 }
 
@@ -458,7 +491,7 @@ BOOST_AUTO_TEST_CASE(problem_getters_test)
     sparsity_pattern grads_2_correct{{0,0},{0,1}};
     std::vector<vector_double> hess_22{{1,1},{1,1}};
     std::vector<sparsity_pattern> hesss_22_correct{{{0,0},{1,0}},{{0,0},{1,0}}};
-    
+
     problem p1{base_p(2,3,4,{3,4,5,6,7,8,9,0,1}, lb_2, ub_2)};
     problem p2{full_p(2,0,0,fit_2,lb_2,ub_2,grad_2, grads_2_correct,hess_22, hesss_22_correct)};
     problem p3{empty{}};
@@ -492,4 +525,40 @@ BOOST_AUTO_TEST_CASE(problem_getters_test)
     // Default
     BOOST_CHECK(p3.get_name() == typeid(*p3.extract<empty>()).name());
     BOOST_CHECK(p3.get_extra_info() == "");
+}
+
+BOOST_AUTO_TEST_CASE(problem_serialization_test)
+{
+    // Do the checking with the full problem.
+    problem p{full_p{}}, p2{p};
+    // Call objfun, grad and hess to increase the internal counters.
+    p.fitness({1.});
+    p.gradient({1.});
+    p.hessians({1.});
+    // Store the string representation.
+    std::stringstream ss;
+    stream(ss,p);
+    const std::string cmp = ss.str();
+    ss.str("");
+    // Now serialize, deserialize and compare the result.
+    {
+    cereal::JSONOutputArchive oarchive(ss);
+    oarchive(p);
+    }
+    // Change the content of p before deserializing.
+    p = problem{grad_p{}};
+    {
+    cereal::JSONInputArchive iarchive(ss);
+    iarchive(p);
+    }
+    std::ostringstream ss2;
+    stream(ss2,p);
+    BOOST_CHECK_EQUAL(ss2.str(),cmp);
+    // Check that the properties of base_p where restored as well.
+    BOOST_CHECK_EQUAL(p.extract<full_p>()->m_nobj,p2.extract<full_p>()->m_nobj);
+    BOOST_CHECK_EQUAL(p.extract<full_p>()->m_nec,p2.extract<full_p>()->m_nec);
+    BOOST_CHECK_EQUAL(p.extract<full_p>()->m_nic,p2.extract<full_p>()->m_nic);
+    BOOST_CHECK(p.extract<full_p>()->m_ret_fit == p2.extract<full_p>()->m_ret_fit);
+    BOOST_CHECK(p.extract<full_p>()->m_lb == p2.extract<full_p>()->m_lb);
+    BOOST_CHECK(p.extract<full_p>()->m_ub == p2.extract<full_p>()->m_ub);
 }
