ac0e49f6f6e97cece84bac004c4764ae9e983509
Dario Izzo <dario.izzo@gmail.com>
2016-04-21 22:05:17 +0200

    test  translate finished

--- tests/translate.cpp
+++ tests/translate.cpp
@@ -14,26 +14,27 @@
 
 using namespace pagmo;
 
-BOOST_AUTO_TEST_CASE(translate_construction)
+BOOST_AUTO_TEST_CASE(translate_construction_test)
 {
     // First we check directly the two constructors
-    {
-        translate p0{};
-        translate p1{null_problem{},{1}};
+    translate p0{};
+    translate p1{null_problem{},{1}};
 
-        auto p0_string = boost::lexical_cast<std::string>(p0);
-        auto p1_string = boost::lexical_cast<std::string>(p1);
+    auto p0_string = boost::lexical_cast<std::string>(p0);
+    auto p1_string = boost::lexical_cast<std::string>(p1);
 
-        // We check that the default constructor constructs a problem
-        // which has an identical representation to the problem
-        // built by the explicit constructor.
-        BOOST_CHECK(p0_string==p1_string);
+    // We check that the default constructor constructs a problem
+    // which has an identical representation to the problem
+    // built by the explicit constructor.
+    BOOST_CHECK(p0_string==p1_string);
 
-        // We check that wrong size for translation results in an invalid_argument
-        // exception
-        BOOST_CHECK_THROW((translate{null_problem{},{1,2}}), std::invalid_argument);
-    }
+    // We check that wrong size for translation results in an invalid_argument
+    // exception
+    BOOST_CHECK_THROW((translate{null_problem{},{1,2}}), std::invalid_argument);
+}
 
+BOOST_AUTO_TEST_CASE(translate_functional_test)
+{
     // Then we check that the hock_schittkowsky_71 problem is actually translated
     {
         problem p0{hock_schittkowsky_71{}};
@@ -53,7 +54,7 @@ BOOST_AUTO_TEST_CASE(translate_construction)
             BOOST_CHECK_CLOSE(bounds0.first[i], bounds2.first[i], 1e-13);
             BOOST_CHECK_CLOSE(bounds0.second[i], bounds2.second[i], 1e-13);
         }
-        // We check that name has [translated] appended
+        // We check that the problem's name has [translated] appended
         BOOST_CHECK(p1.get_name().find("[translated]") != std::string::npos);
         // We check that extra info has "Translation Vector:" somewhere"
         BOOST_CHECK(p1.get_extra_info().find("Translation Vector:") != std::string::npos);
@@ -61,5 +62,32 @@ BOOST_AUTO_TEST_CASE(translate_construction)
         auto translationvector = p1.extract<translate>()->get_translation();
         BOOST_CHECK((translationvector == vector_double{ 0.1,-0.2, 0.3, 0.4}));
     }
+}
 
+BOOST_AUTO_TEST_CASE(translate_serialization_test)
+{
+    // Do the checking with the full problem.
+    problem p0{hock_schittkowsky_71{}};
+    problem p{translate{p0,{ 0.1,-0.2, 0.3, 0.4}}};
+    // Call objfun, grad and hess to increase
+    // the internal counters.
+    p.fitness({1.,1.,1.,1.});
+    p.gradient({1.,1.,1.,1.});
+    p.hessians({1.,1.,1.,1.});
+    // Store the string representation of p.
+    std::stringstream ss;
+    auto before = boost::lexical_cast<std::string>(p);
+    // Now serialize, deserialize and compare the result.
+    {
+    cereal::JSONOutputArchive oarchive(ss);
+    oarchive(p);
+    }
+    // Change the content of p before deserializing.
+    p = problem{null_problem{}};
+    {
+    cereal::JSONInputArchive iarchive(ss);
+    iarchive(p);
+    }
+    auto after = boost::lexical_cast<std::string>(p);
+    BOOST_CHECK_EQUAL(before, after);
 }
