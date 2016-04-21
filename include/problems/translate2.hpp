580fd824c8ccaa52be25b3bb1262ba02a7f53a2c
Dario Izzo <dario.izzo@gmail.com>
2016-04-21 09:50:05 +0200

    translate option 2 refined and accepted

--- include/problems/translate2.hpp
+++ include/problems/translate2.hpp
@@ -17,11 +17,11 @@ namespace pagmo
  * This pagmo::problem translates the whole search space of an input 
  * pagmo::problem by a fixed translation vector. 
  */
-class translate : public problem
+class translate
 {
 public:
     /// Default constructor
-    translate() : problem(null{}), m_translation({1.}) {}
+    translate():m_p(problem{null{}}),m_translation({1.}) {}
 
     /// Constructor from user-defined problem and translation vector
     /**
@@ -36,24 +36,42 @@ public:
      * not equal to the problem dimension \f$ n_x\f$.
      * @throws unspecified any exception thrown by the pagmo::problem constructor
      */
-    //template <typename T>
-    //explicit translate(T &&p, const vector_double &translation) : problem(p), m_translation(translation)    {
-    //    if (translation.size() != get_nx()) {
-    //        pagmo_throw(std::invalid_argument,"Length of shift vector is: " + std::to_string(translation.size()) + " while the problem dimension is: " + std::to_string(get_nx()));
-    //    }
-   // }
+    template <typename T>
+    explicit translate(T &&p, const vector_double &translation) : m_p(std::forward<T>(p)), m_translation(translation)    {
+        if (translation.size() != m_p.get_nx()) {
+            pagmo_throw(std::invalid_argument,"Length of shift vector is: " + std::to_string(translation.size()) + " while the problem dimension is: " + std::to_string(m_p.get_nx()));
+        }
+    }
 
     /// Fitness of the translated problem
     vector_double fitness(const vector_double &x) const
     {
         vector_double x_deshifted = translate_back(x);
-        return dynamic_cast<const problem*>(this)->fitness(x_deshifted);
+        return m_p.fitness(x_deshifted);
     }
- 
+
+    /// Number of objectives (unchanged)
+    vector_double::size_type get_nobj() const
+    {
+        return m_p.get_nobj();
+    }
+
+    /// Equality constraint dimension (unchanged)
+    vector_double::size_type get_nec() const
+    {
+        return m_p.get_nec();
+    }
+
+    /// Inequality constraint dimension (unchanged)
+    vector_double::size_type get_nic() const
+    {
+        return m_p.get_nic();
+    }
+    
     /// Problem bounds of the translated problem
     std::pair<vector_double, vector_double> get_bounds() const
     {
-        auto b_sh = dynamic_cast<const problem*>(this)->get_bounds();
+        auto b_sh = m_p.get_bounds();
         return {apply_translation(b_sh.first), apply_translation(b_sh.second)};
     }
 
@@ -61,22 +79,32 @@ public:
     vector_double gradient(const vector_double &x) const
     {
         vector_double x_deshifted = translate_back(x);
-        return dynamic_cast<const problem*>(this)->gradient(x_deshifted);
+        return m_p.gradient(x_deshifted);
     }
 
+    /// Gradient sparsity of the translated problem
+    sparsity_pattern gradient_sparsity() const
+    {
+        return m_p.gradient_sparsity();
+    }
 
     /// Hessians of the translated problem
-    std::vector<vector_double> hessians(const vector_double &x) const
+    std::vector<vector_doubl e> hessians(const vector_double &x) const
     {
         vector_double x_deshifted = translate_back(x);
-        return dynamic_cast<const problem*>(this)->hessians(x_deshifted);
+        return m_p.hessians(x_deshifted);
     }
 
+    /// Hessian sparsity of the translated problem
+    std::vector<sparsity_pattern> hessians_sparsity() const
+    {
+        return m_p.hessians_sparsity();
+    }
 
     /// Appends "[shifted]" to the user-defined problem name
     std::string get_name() const
     {   
-        return dynamic_cast<const problem*>(this)->get_name() + " [shifted]";
+        return m_p.get_name() + " [shifted]";
     }
 
     /// Extra informations
@@ -84,7 +112,7 @@ public:
     {
         std::ostringstream oss;
         stream(oss, m_translation);
-        return dynamic_cast<const problem*>(this)->get_extra_info() + "\n\tTranslation Vector: " + oss.str();
+        return m_p.get_extra_info() + "\n\tTranslation Vector: " + oss.str();
     }
     
     /// Gets the translation vector
@@ -97,7 +125,31 @@ public:
     template <typename Archive>
     void serialize(Archive &ar) 
     {
-        ar(m_translation);
+        ar(m_p, m_translation);
+    }
+
+    /// Sets problem::has_gradient to the value of the user-defined problem's
+    bool has_gradient() const
+    {
+        return m_p.has_gradient();
+    }
+
+    /// Sets problem::has_hessians to the value of the user-defined problem's
+    bool has_hessians() const
+    {
+        return m_p.has_hessians();
+    }
+
+    /// Sets problem::has_gradient_sparsity to the value of the user-defined problem's
+    bool has_gradient_sparsity() const
+    {
+        return m_p.has_gradient_sparsity();
+    }
+
+    /// Sets problem::has_hessians_sparsity to the value of the user-defined problem's
+    bool has_hessians_sparsity() const
+    {
+        return m_p.has_hessians_sparsity();
     }
 
 private:
@@ -117,12 +169,12 @@ private:
         return x_sh;
     }
 
-    /// translation vector
+    problem m_p;
     vector_double m_translation;
 };
 
 }
 
-//PAGMO_REGISTER_PROBLEM(pagmo::translate)
+PAGMO_REGISTER_PROBLEM(pagmo::translate)
 
 #endif
