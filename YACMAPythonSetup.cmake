ca34c61173bd7275c526f64e2c7ff0a7218c3e4b
Francesco Biscani <bluescarni@gmail.com>
2016-04-23 12:53:08 +0200

    Some misc minor changes and fixes.

--- YACMAPythonSetup.cmake
+++ YACMAPythonSetup.cmake
@@ -24,7 +24,10 @@ set(YACMA_PY_MODULE_EXTENSION "")
 # Determine the installation path for Python modules.
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     message(STATUS "OS X platform detected.")
-    # TODO
+    # Apparently on OS X Python expects the .so extension for compiled modules.
+	message(STATUS "Output extension for compiled modules will be '.so'.")
+    set(YACMA_PY_MODULE_EXTENSION "so")
+    # TODO fill in.
 elseif(UNIX)
     message(STATUS "Generic UNIX platform detected.")
     # We must establish if the installation dir for Python modules is named 'site-packages' (as usual)
@@ -37,12 +40,13 @@ elseif(WIN32)
     message(STATUS "Windows platform detected.")
     message(STATUS "Output extension for compiled modules will be '.pyd'.")
     set(YACMA_PY_MODULE_EXTENSION "pyd")
-    # TODO
+    # TODO fill in.
 endif()
 
 message(STATUS "Python modules install path: " "${YACMA_PYTHON_MODULES_INSTALL_PATH}")
 
 macro(YACMA_PYTHON_MODULE name)
+    message(STATUS "Setting up the compilation of the Python module \"${name}\".")
     # A Python module is a shared library.
     add_library("${name}" SHARED ${ARGN})
     # Any "lib" prefix normally added by CMake must be removed.
@@ -56,8 +60,10 @@ macro(YACMA_PYTHON_MODULE name)
     # with clang and gcc. See:
     # https://bugs.python.org/issue11149
     # http://www.python.org/dev/peps/pep-3123/
+    # NOTE: not sure here how we should set flags up for MSVC or clang on windows, need
+    # to check in the future.
     if(YACMA_COMPILER_IS_GNUCXX OR YACMA_COMPILER_IS_CLANGXX)
-        message(STATUS "Setting up extra compiler flag \"-fwrapv\" for the Python module \"${name}.\"")
+        message(STATUS "Setting up extra compiler flag \"-fwrapv\" for the Python module \"${name}\".")
         set_target_properties("${name}" PROPERTIES COMPILE_FLAGS "-fwrapv")
         set_target_properties("${name}" PROPERTIES LINK_FLAGS "-fwrapv")
         if(${PYTHON_VERSION_MAJOR} LESS 3)
