cec6d0eca30a69c29f2209793795515102cdcee0
Dario Izzo <dario.izzo@gmail.com>
2016-05-03 21:43:21 +0200

    header sanitized

--- cmake_modules/yacma/YACMACompilerLinkerSettings.cmake
+++ cmake_modules/yacma/YACMACompilerLinkerSettings.cmake
@@ -101,3 +101,10 @@ endmacro()
 
 # Mark as included.
 set(YACMACompilerLinkerSettingsIncluded YES)
+
+# This is an OS X specific setting that is suggested to be enabled. See:
+# https://blog.kitware.com/upcoming-in-cmake-2-8-12-osx-rpath-support/
+# http://stackoverflow.com/questions/31561309/cmake-warnings-under-os-x-macosx-rpath-is-not-specified-for-the-following-targe
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    set(CMAKE_MACOSX_RPATH 1)
+endif()
