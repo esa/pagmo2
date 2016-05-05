54b05fb33eda926879a8fc12d24ad81e20e85e0f
Francesco Biscani <bluescarni@gmail.com>
2016-05-05 19:40:34 +0200

    Implement and test RNG serialization.

--- include/serialization.hpp
+++ include/serialization.hpp
@@ -22,4 +22,43 @@
     #pragma GCC diagnostic pop
 #endif
 
+#include <cstddef>
+#include <random>
+#include <sstream>
+#include <string>
+
+// Implement serialization for the Mersenne twister engine.
+namespace cereal
+{
+
+template <class Archive, class UIntType,
+    std::size_t w, std::size_t n, std::size_t m, std::size_t r,
+    UIntType a, std:: size_t u, UIntType d, std::size_t s,
+    UIntType b, std::size_t t, UIntType c, std::size_t l, UIntType f> inline
+void CEREAL_SAVE_FUNCTION_NAME( Archive & ar, std::mersenne_twister_engine<UIntType,w,n,m,r,a,u,d,s,b,t,c,l,f> const & e )
+{
+    std::ostringstream oss;
+    // Use the "C" locale.
+    oss.imbue(std::locale::classic());
+    oss << e;
+    ar(oss.str());
+}
+
+template <class Archive, class UIntType,
+    std::size_t w, std::size_t n, std::size_t m, std::size_t r,
+    UIntType a, std:: size_t u, UIntType d, std::size_t s,
+    UIntType b, std::size_t t, UIntType c, std::size_t l, UIntType f> inline
+void CEREAL_LOAD_FUNCTION_NAME( Archive & ar, std::mersenne_twister_engine<UIntType,w,n,m,r,a,u,d,s,b,t,c,l,f> & e )
+{
+    std::istringstream iss;
+    // Use the "C" locale.
+    iss.imbue(std::locale::classic());
+    std::string tmp;
+    ar(tmp);
+    iss.str(tmp);
+    iss >> e;
+}
+
+}
+
 #endif
---
+++ tests/rng_serialization.cpp
@@ -0,0 +1,49 @@
+#define BOOST_TEST_MODULE pagmo_rng_serialization_test
+#include <boost/test/unit_test.hpp>
+
+#include <iterator>
+#include <random>
+#include <sstream>
+#include <vector>
+
+#include "../include/serialization.hpp"
+
+static std::mt19937 rng;
+
+static const int ntrials = 100;
+
+BOOST_AUTO_TEST_CASE(rng_serialization_test)
+{
+    using r_type = std::mt19937;
+    using ia_type = cereal::JSONInputArchive;
+    using oa_type = cereal::JSONOutputArchive;
+    auto rng_save = [](const r_type &r) {
+        std::stringstream ss;
+        {
+        oa_type oarchive(ss);
+        oarchive(r);
+        }
+        return ss.str();
+    };
+    auto rng_load = [](const std::string &str, r_type &r) {
+        std::stringstream ss;
+        ss.str(str);
+        {
+        ia_type iarchive(ss);
+        iarchive(r);
+        }
+    };
+    std::uniform_int_distribution<r_type::result_type> dist;
+    for (auto i = 0; i < ntrials; ++i) {
+        auto seed = dist(rng);
+        r_type r;
+        r.seed(seed);
+        auto str = rng_save(r);
+        std::vector<r_type::result_type> v1;
+        std::generate_n(std::back_inserter(v1),100,r);
+        rng_load(str,r);
+        std::vector<r_type::result_type> v2;
+        std::generate_n(std::back_inserter(v2),100,r);
+        BOOST_CHECK_EQUAL_COLLECTIONS(v1.begin(),v1.end(),v2.begin(),v2.end());
+    }
+}
