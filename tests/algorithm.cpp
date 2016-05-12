8d80609149df1e23338934cca0fd28cb70fd283b
Dario Izzo <dario.izzo@gmail.com>
2016-05-12 20:35:29 +0200

    [ci skip] copy and move assignment test added to algorithm

--- tests/algorithm.cpp
+++ tests/algorithm.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "../include/algorithm.hpp"
+#include "../include/algorithms/null_algorithm.hpp"
 #include "../include/problems/rosenbrock.hpp"
 #include "../include/population.hpp"
 #include "../include/serialization.hpp"
@@ -17,7 +18,7 @@
 using namespace pagmo;
 
 // Complete algorithm stochastic
-struct al_01 
+struct al_01
 {
     al_01() {};
     population evolve(const population& pop) const {return pop;};
@@ -30,7 +31,7 @@ struct al_01
 };
 
 // Minimal algorithm deterministic
-struct al_02 
+struct al_02
 {
     al_02() {};
     population evolve(const population& pop) const {return pop;};
@@ -38,7 +39,7 @@ struct al_02
 
 BOOST_AUTO_TEST_CASE(algorithm_construction_test)
 {
-    // We construct two different algorithms. One having all the 
+    // We construct two different algorithms. One having all the
     // mandatory and optional methods implemented, the other
     // having only the mandatory methods implemented
     algorithm algo_full{al_01{}};
@@ -109,7 +110,7 @@ BOOST_AUTO_TEST_CASE(algorithm_move_constructor_test)
 }
 
 // Algorithm with overrides
-struct al_03 
+struct al_03
 {
     al_03() {};
     population evolve(const population& pop) const {return pop;};
@@ -128,4 +129,56 @@ BOOST_AUTO_TEST_CASE(algorithm_override_mechanics_test)
     algorithm algo{al_03{}};
     BOOST_CHECK(algo.has_set_seed() == false);
     BOOST_CHECK(algo.has_set_verbosity() == false);
+}
+
+BOOST_AUTO_TEST_CASE(algorithm_move_assignment_test)
+{
+    // We instantiate an algorithm
+    algorithm algo{al_01{}};
+
+    // We set the seed and verbosity so that the default values are changed
+    algo.set_seed(1u);
+    algo.set_verbosity(1u);
+
+    // We store a streaming representation of the object
+    auto algo_string = boost::lexical_cast<std::string>(algo);
+    // We get the memory address where the user algo is stored
+    auto a1 = algo.extract<al_01>();
+    // We call the move assignment
+    algorithm moved_algo{null_algorithm{}};
+    moved_algo = std::move(algo);
+    // We get the memory address where the user algo is stored
+    auto a2 = moved_algo.extract<al_01>();
+    // And the string representation of the moved algo
+    auto moved_algo_string = boost::lexical_cast<std::string>(moved_algo);
+    // 1 - We check the resource pointed by m_ptr has been moved from algo to moved_algo
+    BOOST_CHECK(a1==a2);
+    // 2 - We check that the two string representations are identical
+    BOOST_CHECK(algo_string==moved_algo_string);
+}
+
+BOOST_AUTO_TEST_CASE(algorithm_copy_assignment_test)
+{
+    // We check the copy constructor
+    algorithm algo{al_01{}};
+
+    // We set the seed and verbosity so that the default values are changed
+    algo.set_seed(1u);
+    algo.set_verbosity(1u);
+
+    // We call the copy assignment opeator
+    algorithm algo_copy{null_algorithm{}};
+    algo_copy = algo;
+    // We extract the user algorithm
+    auto a1 = algo.extract<al_01>();
+    auto a2 = algo_copy.extract<al_01>();
+
+    // 1 - We check the resources pointed to by m_ptr have different address
+    BOOST_CHECK(a1!=0);
+    BOOST_CHECK(a2!=0);
+    BOOST_CHECK(a1!=a2);
+    // 2 - We check that the other members are copied
+    BOOST_CHECK(algo.get_name() == algo_copy.get_name());
+    BOOST_CHECK(algo.has_set_seed() == algo_copy.has_set_seed());
+    BOOST_CHECK(algo.has_set_verbosity() == algo_copy.has_set_verbosity());
 }
