a97052a3ba63268729c3618f9feb458c5bab23b3
Dario Izzo <dario.izzo@gmail.com>
2016-05-18 11:12:07 +0200

    First user-algorithm draft proposal finished

--- tests/sea.cpp
+++ tests/sea.cpp
@@ -6,17 +6,23 @@
 
 
 #include "../include/algorithms/sea.hpp"
+#include "../include/io.hpp"
 #include "../include/problems/rosenbrock.hpp"
 #include "../include/population.hpp"
 #include "../include/types.hpp"
 
 using namespace pagmo;
+using namespace std;
 
 BOOST_AUTO_TEST_CASE(sea_algorithm_test)
 {
-    problem prob{rosenbrock{50u}};
-    population pop{prob, 5u};
-    algorithm algo(sea{100000u});
+    problem prob{rosenbrock{2u}};
+    population pop{prob, 5u, 23u};
+    algorithm algo(sea{1000u, 23u});
     algo.set_verbosity(1u);
+    print(algo, '\n');
     pop = algo.evolve(pop);
+    for ( const auto& i : algo.extract<sea>()->get_log()) {
+      cout << get<0>(i) << ", " << get<1>(i) << ", " << get<2>(i) << ", " << get<3>(i) << endl;
+    }
 }
