e28b32c3883759b2bc335bed06b3bd8ebd4c0f48
Dario Izzo <dario.izzo@gmail.com>
2016-06-21 09:49:21 +0200

    first commit

---
+++ include/algorithms/moead.hpp
@@ -0,0 +1,28 @@
+#ifndef PAGMO_ALGORITHMS_MOEAD_HPP
+#define PAGMO_ALGORITHMS_MOEAD_HPP
+
+#include <iomanip>
+#include <random>
+#include <string>
+#include <tuple>
+
+#include "../algorithm.hpp" // needed for the cereal macro
+#include "../io.hpp"
+#include "../exceptions.hpp"
+#include "../population.hpp"
+#include "../rng.hpp"
+
+namespace pagmo
+{
+
+class cmaes
+{
+public:
+
+};
+
+} //namespace pagmo
+
+// PAGMO_REGISTER_ALGORITHM(pagmo::moead)
+
+#endif
