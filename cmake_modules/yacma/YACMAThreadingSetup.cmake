d1c4117d6f50c67ad5a694b032c20fb68a83d4a3
Francesco Biscani <bluescarni@gmail.com>
2017-01-09 23:51:51 +0100

    yacma: included updated FindBoost and rework the threading setup.

--- cmake_modules/yacma/YACMAThreadingSetup.cmake
+++ cmake_modules/yacma/YACMAThreadingSetup.cmake
@@ -1,74 +1,64 @@
-# NOTE: it looks like on CMake >= 3.1 there's a thread import target that can be used:
-# https://cmake.org/cmake/help/v3.5/module/FindThreads.html
-# http://stackoverflow.com/questions/1620918/cmake-and-libpthread
-
 if(YACMAThreadingSetupIncluded)
     return()
 endif()
 
-include(CheckCXXCompilerFlag)
-include(CheckCXXSymbolExists)
+include(YACMACompilerLinkerSettings)
 
-# Initial thread setup.
+# Initial thread setup. See FindThreads.cmake for an explanation.
+set(THREADS_PREFER_PTHREAD_FLAG YES)
 find_package(Threads REQUIRED)
+unset(THREADS_PREFER_PTHREAD_FLAG)
 message(STATUS "Thread library: ${CMAKE_THREAD_LIBS_INIT}")
 
 # Setup variable for threading-specific flags.
-set(YACMA_THREADING_CXX_FLAGS "")
+set(YACMA_THREADING_CXX_FLAGS)
 
 # POSIX thread setup. Intended both for UNIX and Windows (the latter when using some sort of
 # pthread emulation/wrapper like pthreads-win32).
 if(CMAKE_USE_PTHREADS_INIT)
 	message(STATUS "POSIX threads detected.")
-	# For POSIX threads, we try to see if the compiler accepts the -pthread flag. It is a bit of a kludge,
-	# but I do not have any better idea at the moment. The situation is hairy, e.g.,
-	# different systems require different GCC flags:
-	# http://gcc.gnu.org/onlinedocs/libstdc++/manual/using_concurrency.html
-	CHECK_CXX_COMPILER_FLAG(-pthread YACMA_PTHREAD_COMPILER_FLAG)
-	# NOTE: we do not enable the -pthread flag on OS X as it is apparently ignored.
-	if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND YACMA_PTHREAD_COMPILER_FLAG)
-		message(STATUS "Enabling the -pthread compiler flag.")
-		# NOTE: according to GCC docs, this sets the flag for both compiler and linker. This should
-		# work similarly for clang as well.
-		set(YACMA_THREADING_CXX_FLAGS "-pthread")
-	endif()
-	unset(YACMA_PTHREAD_COMPILER_FLAG)
+	# Detect affinity setting primitives.
+	include(CheckCXXSymbolExists)
 	set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}")
 	CHECK_CXX_SYMBOL_EXISTS("pthread_setaffinity_np" "pthread.h" _YACMA_HAS_PTHREAD_SETAFFINITY)
 	CHECK_CXX_SYMBOL_EXISTS("pthread_getaffinity_np" "pthread.h" _YACMA_HAS_PTHREAD_GETAFFINITY)
 	unset(CMAKE_REQUIRED_LIBRARIES)
 	if(_YACMA_HAS_PTHREAD_SETAFFINITY AND _YACMA_HAS_PTHREAD_GETAFFINITY)
-		set(YACMA_HAS_PTHREAD_AFFINITY true)
+		set(YACMA_HAS_PTHREAD_AFFINITY YES)
 	else()
-		set(YACMA_HAS_PTHREAD_AFFINITY false)
+		set(YACMA_HAS_PTHREAD_AFFINITY NO)
 	endif()
 	if(YACMA_HAS_PTHREAD_AFFINITY)
 		message(STATUS "POSIX threads affinity extensions detected.")
 	else()
-		message(STATUS "POSIX threads affinity extensions not detected.")
+		message(STATUS "POSIX threads affinity extensions NOT detected.")
 	endif()
 endif()
 
-if(MINGW AND NOT CMAKE_USE_PTHREADS_INIT)
-	# NOTE: the idea here is that the -mthreads flag is useful only when using the native Windows
-	# threads, apparently if we are using some pthread variant it is not needed:
-	# http://mingw-users.1079350.n2.nabble.com/pthread-vs-mthreads-td7114500.html
-	message(STATUS "Native Windows threads detected on MinGW, enabling the -mthreads flag.")
-	# NOTE: the -mthreads flag is needed both in compiling and linking. CMake should pass it down
-	# to the linker even if only set in CXX_FLAGS.
-	set(YACMA_THREADING_CXX_FLAGS "-mthreads")
+if(MINGW)
+	message(STATUS "Enabling the '-mthreads' flag for MinGW.")
+	list(APPEND YACMA_THREADING_CXX_FLAGS "-mthreads")
 endif()
 
-message(STATUS "Extra compiler flags for threading: ${YACMA_THREADING_CXX_FLAGS}")
+if(YACMA_COMPILER_IS_MSVC)
+	message(STATUS "Enabling the '_MT' definition for MSVC.")
+	list(APPEND YACMA_THREADING_CXX_FLAGS "/D_MT")
+endif()
 
-# Detect thread_local availability.
-try_compile(YACMA_HAS_THREAD_LOCAL ${CMAKE_BINARY_DIR}
-	"${CMAKE_CURRENT_LIST_DIR}/yacma_thread_local_tests.cpp")
-if(YACMA_HAS_THREAD_LOCAL)
-	message(STATUS "The 'thread_local' keyword is available.")
+# Check if we have thread_local.
+# NOTE: we need to double check what happens with OSX's clang here.
+set(YACMA_HAVE_THREAD_LOCAL)
+list(FIND CMAKE_CXX_COMPILE_FEATURES "cxx_thread_local" YACMA_HAVE_THREAD_LOCAL)
+if(${YACMA_HAVE_THREAD_LOCAL} EQUAL -1)
+	message(STATUS "The 'thread_local' keyword is NOT supported.")
+	set(YACMA_HAVE_THREAD_LOCAL NO)
 else()
-	message(STATUS "The 'thread_local' keyword is not available.")
+	message(STATUS "The 'thread_local' keyword is supported.")
+	set(YACMA_HAVE_THREAD_LOCAL YES)
 endif()
+
+# Final report.
+message(STATUS "Extra compiler flags for threading: ${YACMA_THREADING_CXX_FLAGS}")
 
 # Mark as included.
 set(YACMAThreadingSetupIncluded YES)
--- cmake_modules/yacma/yacma_thread_local_tests.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-int main()
-{
-    static thread_local std::vector<char> n;
-    std::cout << n.size() << std::endl;
-}
