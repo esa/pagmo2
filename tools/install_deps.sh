b7000f36d5f8bf7476fb43c47ac3d84c674fbd9c
Francesco Biscani <bluescarni@gmail.com>
2017-01-10 23:24:50 +0100

    First bits for travis.

---
+++ tools/install_deps.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+# Echo each command
+set -x
+
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
+else
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+export deps_dir=$HOME/local
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --add channels conda-forge --force
+
+conda_pkgs="boost>=1.55 cmake>=3.2 eigen"
+
+conda create -q -p $deps_dir -y $conda_pkgs
+source activate $deps_dir
+
+set +e
+set +x
---
+++ tools/install_travis.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+# Echo each command
+set -x
+
+export PATH="$deps_dir/bin:$PATH"
+
+if [[ "${PAGMO_BUILD}" == "ReleaseGCC48" ]]; then
+    CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release -DPAGMO_BUILD_TESTS=yes -DPAGMO_BUILD_TUTORIALS=yes -DPAGMO_WITH_EIGEN3=yes ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+elif [[ "${PAGMO_BUILD}" == "DebugGCC48" ]]; then
+    CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DPAGMO_BUILD_TUTORIALS=yes -DPAGMO_WITH_EIGEN3=yes -DCMAKE_CXX_FLAGS="-fsanitize=address"../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+elif [[ "${PAGMO_BUILD}" == "CoverageGCC5" ]]; then
+    CXX=g++-5 CC=gcc-5 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DPAGMO_BUILD_TUTORIALS=yes -DPAGMO_WITH_EIGEN3=yes -DCMAKE_CXX_FLAGS="--coverage"../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+    bash <(curl -s https://codecov.io/bash) -x gcov-5;
+elif [[ "${PAGMO_BUILD}" == "DebugGCC6" ]]; then
+    CXX=g++-6 CC=gcc-6 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DPAGMO_BUILD_TUTORIALS=yes -DPAGMO_WITH_EIGEN3=yes ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+elif [[ "${PAGMO_BUILD}" == "DebugClang38" ]]; then
+    CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DPAGMO_BUILD_TUTORIALS=yes -DPAGMO_WITH_EIGEN3=yes ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+elif [[ "${PAGMO_BUILD}" == "ReleaseClang38" ]]; then
+    CXX=clang++-3.8 CC=clang-3.8 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release -DPAGMO_BUILD_TESTS=yes -DPAGMO_BUILD_TUTORIALS=yes -DPAGMO_WITH_EIGEN3=yes ../;
+    make -j2 VERBOSE=1;
+    ctest -V;
+fi
+
+set +e
+set +x
