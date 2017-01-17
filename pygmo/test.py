6bede20df72fb335ce169ea1da8f26fd73b8e6f8
Francesco Biscani <bluescarni@gmail.com>
2017-01-17 21:06:14 +0100

    Revert "Add copyright notices on source files."
    This reverts commit 5fdbb0d145edb7e5a940f8be3c5c3254b316a5fd.

--- pygmo/test.py
+++ pygmo/test.py
@@ -1,25 +1,5 @@
 # -*- coding: iso-8859-1 -*-
 
-# Copyright 2017 PaGMO development team
-# This file is part of the PaGMO library.
-# The PaGMO library is free software; you can redistribute it and/or modify
-# it under the terms of either:
-#   * the GNU Lesser General Public License as published by the Free
-#     Software Foundation; either version 3 of the License, or (at your
-#     option) any later version.
-# or
-#   * the GNU General Public License as published by the Free Software
-#     Foundation; either version 3 of the License, or (at your option) any
-#     later version.
-# or both in parallel, as here.
-# The PaGMO library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-# You should have received copies of the GNU General Public License and the
-# GNU Lesser General Public License along with the PaGMO library.  If not,
-# see https://www.gnu.org/licenses/.
-
 from __future__ import absolute_import as _ai
 
 import unittest as _ut
--- tests/population.cpp
+++ tests/population.cpp
@@ -1,23 +1,3 @@
-/* Copyright 2017 PaGMO development team
-This file is part of the PaGMO library.
-The PaGMO library is free software; you can redistribute it and/or modify
-it under the terms of either:
-  * the GNU Lesser General Public License as published by the Free
-    Software Foundation; either version 3 of the License, or (at your
-    option) any later version.
-or
-  * the GNU General Public License as published by the Free Software
-    Foundation; either version 3 of the License, or (at your option) any
-    later version.
-or both in parallel, as here.
-The PaGMO library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
-You should have received copies of the GNU General Public License and the
-GNU Lesser General Public License along with the PaGMO library.  If not,
-see https://www.gnu.org/licenses/. */
-
 #define BOOST_TEST_MODULE population_test
 
 #include <boost/lexical_cast.hpp>
