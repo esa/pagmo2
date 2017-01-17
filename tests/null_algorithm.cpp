5fdbb0d145edb7e5a940f8be3c5c3254b316a5fd
Francesco Biscani <bluescarni@gmail.com>
2017-01-17 13:21:45 +0100

    Add copyright notices on source files.

--- tests/null_algorithm.cpp
+++ tests/null_algorithm.cpp
@@ -1,3 +1,23 @@
+/* Copyright 2017 PaGMO development team
+This file is part of the PaGMO library.
+The PaGMO library is free software; you can redistribute it and/or modify
+it under the terms of either:
+  * the GNU Lesser General Public License as published by the Free
+    Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+or
+  * the GNU General Public License as published by the Free Software
+    Foundation; either version 3 of the License, or (at your option) any
+    later version.
+or both in parallel, as here.
+The PaGMO library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+You should have received copies of the GNU General Public License and the
+GNU Lesser General Public License along with the PaGMO library.  If not,
+see https://www.gnu.org/licenses/. */
+
 #define BOOST_TEST_MODULE null_algo_test
 #include <boost/test/included/unit_test.hpp>
 
