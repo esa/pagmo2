be26a59ce211035e05d571b96172a4806e82a27c
Marcus Märtens <mmarcusx@gmail.com>
2017-02-04 19:28:41 +0100

    added numerical testcases to hypervolume test-suite

--- tests/hypervolume.cpp
+++ tests/hypervolume.cpp
@@ -31,6 +31,10 @@ see https://www.gnu.org/licenses/. */
 #include <exception>
 #include <tuple>
 
+#include <fstream>
+#include <sstream>
+#include <string>
+
 #include <pagmo/detail/hypervolume_all.hpp>
 #include <pagmo/population.hpp>
 #include <pagmo/problem.hpp>
@@ -53,6 +57,109 @@ void assertContribs(const std::vector<vector_double> &points, std::vector<double
 		BOOST_CHECK((hv.exclusive(i, ref) == answers[i]));
 	}
 }
+
+
+class hypervolume_test
+{
+public:
+	hypervolume_test(std::istream &input, std::string test_type, std::string method_name, double eps)
+		: m_input(input), m_test_type(test_type), m_eps(eps) {
+
+		// determine method
+		if (method_name == "hv2d") {
+			m_method = hv2d().clone();
+		}
+		else if (method_name == "hv3d") {
+			m_method = hv3d().clone();
+		}
+		else if (method_name == "wfg") {
+			m_method = hvwfg().clone();
+		}
+		else {
+			// The specified algorithm is not available
+			BOOST_CHECK((false));
+		}
+
+	}
+
+	void run_test() {
+		m_input >> m_num_tests;
+
+		//int OK_counter = 0;
+		for (int t = 0; t < m_num_tests; ++t) {
+			load_common();
+			hypervolume hv_obj = hypervolume(m_points, true);
+
+			// run correct test
+			if (m_test_type == "compute") {
+				load_compute();
+				double hypvol = hv_obj.compute(m_ref_point, m_method);
+				BOOST_CHECK((std::abs(hypvol - m_hv_ans) < m_eps));
+			} else if (m_test_type == "exclusive") {
+				load_exclusive();
+				double hypvol = hv_obj.exclusive(m_p_idx, m_ref_point, m_method);
+				BOOST_CHECK((std::abs(hypvol - m_hv_ans) < m_eps));
+			} else if (m_test_type == "least_contributor") {
+				load_least_contributor();
+				unsigned int point_idx = hv_obj.least_contributor(m_ref_point, m_method);
+				BOOST_CHECK((point_idx == m_idx_ans));
+			} else if (m_test_type == "greatest_contributor") {
+				load_least_contributor(); // loads the same data as least contributor
+				unsigned int point_idx = hv_obj.greatest_contributor(m_ref_point, m_method);
+				BOOST_CHECK((point_idx == m_idx_ans));
+			} else {
+				// The specified computational method is not available (what do you want to compute?)
+				BOOST_CHECK((false));
+			}
+		}
+	}
+
+private:
+	void load_common()
+	{
+		m_input >> m_f_dim >> m_num_points;
+		m_points = std::vector<vector_double>(m_num_points, vector_double(m_f_dim, 0.0));
+		m_ref_point = vector_double(m_f_dim, 0.0);
+		
+		for (int d = 0; d < m_f_dim; ++d) {
+			m_input >> m_ref_point[d];
+		}
+		
+		for (int i = 0; i < m_num_points; ++i) {
+			for (int d = 0; d < m_f_dim; ++d) {
+				m_input >> m_points[i][d];
+			}
+		}
+	}
+
+	void load_compute()
+	{
+		m_input >> m_hv_ans;
+	}
+
+	void load_exclusive()
+	{
+		m_input >> m_p_idx;
+		m_input >> m_hv_ans;
+	}
+
+	void load_least_contributor()
+	{
+		m_input >> m_idx_ans;
+	}
+
+	int m_num_tests, m_f_dim, m_num_points, m_p_idx;
+	unsigned int m_idx_ans;
+	double m_hv_ans;
+	vector_double m_ref_point;
+	std::vector<vector_double> m_points;
+
+	std::shared_ptr<hv_algorithm> m_method;
+	std::istream &m_input;
+	std::string m_test_type;
+	double m_eps;
+};
+
 
 
 BOOST_AUTO_TEST_CASE(hypervolume_compute_test)
@@ -80,7 +187,7 @@ BOOST_AUTO_TEST_CASE(hypervolume_compute_test)
 	// 2d computation of hypervolume indicator
 	hv = hypervolume{ {1, 2},{2, 1} };
 	BOOST_CHECK((hv.compute({ 3,3 }) == 3));
-
+	
 	// point on the border of refpoint(2D)
 	BOOST_CHECK((hv.compute({ 2,2 }) == 0));
 
@@ -351,3 +458,52 @@ BOOST_AUTO_TEST_CASE(hypervolume_refpoint_test) {
 	BOOST_CHECK((hv.refpoint(-1) == vector_double{ 2, 2 }));
 
 }
+
+BOOST_AUTO_TEST_CASE(hypervolume_test_instances) {
+	/** uses some precomputed fronts and hypervolumes to test if algorithms are correct */
+	std::string line;
+
+	// root directory of the hypervolume data
+	std::string input_data_dir("./hypervolume_test_data/");
+		
+	// root directory of the testcases
+	std::string input_data_testcases_dir(input_data_dir + "testcases/");
+	
+	// load list of testcases
+	std::ifstream ifs;
+	ifs.open((input_data_dir + "testcases_list.txt").c_str());
+		
+
+	if (ifs.is_open()) {
+		while (ifs.good()) {
+			getline(ifs, line);
+			if (line == "" || line[0] == '#')
+				continue;
+			std::stringstream ss(line);
+			
+			std::string test_type;
+			std::string method_name;
+			std::string test_name;
+			double eps;
+			
+			ss >> test_type;
+			ss >> method_name;
+			ss >> test_name;
+			ss >> eps;
+
+			// start reading the testcase
+			std::ifstream input((input_data_testcases_dir + test_name).c_str());
+			if (input.is_open()) {
+				hypervolume_test hvt(input, test_type, method_name, eps);
+				hvt.run_test();
+				input.close();
+			}
+			input.close();
+		}
+		ifs.close();
+	} else {
+		// The testcase file is missing
+		//BOOST_CHECK((false));
+	}
+}
+
