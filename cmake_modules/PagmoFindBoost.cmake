5a5bbae0c59069ab816ab7b5bfb92648f5723b4e
Francesco Biscani <bluescarni@gmail.com>
2017-02-14 21:15:11 +0100

    nth tentative cmake fix.

--- cmake_modules/PagmoFindBoost.cmake
+++ cmake_modules/PagmoFindBoost.cmake
@@ -9,25 +9,22 @@ if(NOT Boost_FOUND)
 endif()
 message(STATUS "Detected Boost version: ${Boost_VERSION}")
 message(STATUS "Boost include dirs: ${Boost_INCLUDE_DIRS}")
-if(NOT _Boost_IMPORTED_TARGETS)
-    # NOTE: CMake's Boost finding module will not provide imported targets for recent Boost versions, as it needs
-    # an explicit mapping specifying the dependencies between the various Boost libs (and this is version-dependent).
-    # If we are here, it means that Boost was correctly found with all the needed components, but the Boost version
-    # found is too recent and imported targets are not available. We will reconstruct them here in order to be able
-    # to link to targets rather than using the variables defined by the FindBoost.cmake module.
-    message(STATUS "The imported Boost targets are not available, creating them manually.")
-    if(NOT TARGET Boost::boost)
-        add_library(Boost::boost INTERFACE IMPORTED)
-        set_target_properties(Boost::boost PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIRS}")
+# Might need to recreate targets if they are missing (e.g., older CMake versions).
+if(NOT TARGET Boost::boost)
+    message(STATUS "The 'Boost::boost' target is missing, creating it.")
+    add_library(Boost::boost INTERFACE IMPORTED)
+    set_target_properties(Boost::boost PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIRS}")
+endif()
+if(NOT TARGET Boost::disable_autolinking)
+    message(STATUS "The 'Boost::disable_autolinking' target is missing, creating it.")
+    add_library(Boost::disable_autolinking INTERFACE IMPORTED)
+    if(WIN32)
+        set_target_properties(Boost::disable_autolinking PROPERTIES INTERFACE_COMPILE_DEFINITIONS "BOOST_ALL_NO_LIB")
     endif()
-    if(NOT TARGET Boost::disable_autolinking)
-        add_library(Boost::disable_autolinking INTERFACE IMPORTED)
-        if(WIN32)
-            set_target_properties(Boost::disable_autolinking PROPERTIES INTERFACE_COMPILE_DEFINITIONS "BOOST_ALL_NO_LIB")
-        endif()
-    endif()
-    foreach(_PAGMO_BOOST_COMPONENT ${_PAGMO_REQUIRED_BOOST_LIBS})
-        message(STATUS "Creating the 'Boost::${_PAGMO_BOOST_COMPONENT}' imported target.")
+endif()
+foreach(_PAGMO_BOOST_COMPONENT ${_PAGMO_REQUIRED_BOOST_LIBS})
+    if(NOT TARGET Boost::${_PAGMO_BOOST_COMPONENT})
+        message(STATUS "The 'Boost::${_PAGMO_BOOST_COMPONENT}' imported target is missing, creating it.")
         string(TOUPPER ${_PAGMO_BOOST_COMPONENT} _PAGMO_BOOST_UPPER_COMPONENT)
         if(Boost_USE_STATIC_LIBS)
             add_library(Boost::${_PAGMO_BOOST_COMPONENT} STATIC IMPORTED)
@@ -39,7 +36,5 @@ if(NOT _Boost_IMPORTED_TARGETS)
         set_target_properties(Boost::${_PAGMO_BOOST_COMPONENT} PROPERTIES
             IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
             IMPORTED_LOCATION "${Boost_${_PAGMO_BOOST_UPPER_COMPONENT}_LIBRARY}")
-    endforeach()
-    # NOTE: the FindBoost macro also sets the Release and Debug counterparts of the properties above.
-    # It seems like it is not necessary for our own uses, but keep it in mind for the future.
-endif()
+    endif()
+endforeach()
