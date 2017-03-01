45bf99fff085dd620d0019d72e598fec3c40e6e9
Francesco Biscani <bluescarni@gmail.com>
2017-03-01 16:41:18 +0100

    Tentative fix for GCC 4.8 on travis.

--- tests/population.cpp
+++ tests/population.cpp
@@ -98,6 +98,12 @@ BOOST_AUTO_TEST_CASE(population_construction_test)
     BOOST_CHECK_EQUAL(pop_to_string(pop_a), pop_to_string(pop_c));
     pop_b = std::move(pop_e);
     BOOST_CHECK_EQUAL(pop_to_string(pop_a), pop_to_string(pop_b));
+    // Self assignments.
+    pop_a = pop_b;
+    pop_a = pop_a;
+    BOOST_CHECK_EQUAL(pop_to_string(pop_a), pop_to_string(pop_b));
+    pop_a = std::move(pop_a);
+    BOOST_CHECK_EQUAL(pop_to_string(pop_a), pop_to_string(pop_b));
 }
 
 BOOST_AUTO_TEST_CASE(population_copy_constructor_test)
