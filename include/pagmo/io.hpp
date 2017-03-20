04e4935231ac985f5d321e40b42e07d3d1d9c150
Francesco Biscani <bluescarni@gmail.com>
2017-03-21 00:54:43 +0100

    Fix missing header.

--- include/pagmo/io.hpp
+++ include/pagmo/io.hpp
@@ -33,6 +33,7 @@ see https://www.gnu.org/licenses/. */
 #include <initializer_list>
 #include <iostream>
 #include <iterator>
+#include <sstream>
 #include <string>
 #include <utility>
 #include <vector>
