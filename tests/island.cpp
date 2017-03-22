daad096c8757dfb2b845131d58a8eb7cff62e759
Francesco Biscani <bluescarni@gmail.com>
2017-03-22 01:20:53 +0100

    Some small initial bits of island testing.

--- tests/island.cpp
+++ tests/island.cpp
@@ -36,19 +36,44 @@ see https://www.gnu.org/licenses/. */
 
 using namespace pagmo;
 
-BOOST_AUTO_TEST_CASE(island_construction)
+BOOST_AUTO_TEST_CASE(island_constructors)
 {
-    island isl(thread_island{}, de{}, rosenbrock{}, 20);
-    for (unsigned i = 0u; i < 1000u; ++i) {
-        isl.evolve();
-    }
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.set_algorithm(pagmo::algorithm{});
-    isl.wait();
+    island isl;
+    BOOST_CHECK(isl.get_algorithm().is<null_algorithm>());
+    BOOST_CHECK(isl.get_population().get_problem().is<null_problem>());
+    BOOST_CHECK(isl.get_population().size() == 0u);
+    auto isl2 = isl;
+    BOOST_CHECK(isl2.get_algorithm().is<null_algorithm>());
+    BOOST_CHECK(isl2.get_population().get_problem().is<null_problem>());
+    BOOST_CHECK(isl2.get_population().size() == 0u);
+    island isl3{de{}, population{rosenbrock{}, 25}};
+    BOOST_CHECK(isl3.get_algorithm().is<de>());
+    BOOST_CHECK(isl3.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl3.get_population().size() == 25u);
+    auto isl4 = isl3;
+    BOOST_CHECK(isl4.get_algorithm().is<de>());
+    BOOST_CHECK(isl4.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl4.get_population().size() == 25u);
+    island isl5{thread_island{}, de{}, population{rosenbrock{}, 26}};
+    BOOST_CHECK(isl5.get_algorithm().is<de>());
+    BOOST_CHECK(isl5.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl5.get_population().size() == 26u);
+    island isl6{de{}, rosenbrock{}, 27};
+    BOOST_CHECK(isl6.get_algorithm().is<de>());
+    BOOST_CHECK(isl6.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl6.get_population().size() == 27u);
+    island isl7{de{}, rosenbrock{}, 27, 123};
+    BOOST_CHECK(isl7.get_algorithm().is<de>());
+    BOOST_CHECK(isl7.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl7.get_population().size() == 27u);
+    BOOST_CHECK(isl7.get_population().get_seed() == 123u);
+    island isl8{thread_island{}, de{}, rosenbrock{}, 28};
+    BOOST_CHECK(isl8.get_algorithm().is<de>());
+    BOOST_CHECK(isl8.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl8.get_population().size() == 28u);
+    island isl9{thread_island{}, de{}, rosenbrock{}, 29, 124};
+    BOOST_CHECK(isl9.get_algorithm().is<de>());
+    BOOST_CHECK(isl9.get_population().get_problem().is<rosenbrock>());
+    BOOST_CHECK(isl9.get_population().size() == 29u);
+    BOOST_CHECK(isl9.get_population().get_seed() == 124u);
 }
