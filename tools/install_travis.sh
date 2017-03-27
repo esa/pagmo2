5a5851973f784048df487bd454c0ba9fb6158e0d
Francesco Biscani <bluescarni@gmail.com>
2017-03-27 21:53:23 +0200

    Sleep a bit more to let ipcluster start up.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -45,14 +45,14 @@ elif [[ "${PAGMO_BUILD}" == "Python36" || "${PAGMO_BUILD}" == "Python27" ]]; the
     make install VERBOSE=1;
     ipcluster start --daemonize=True;
     # Give some time for the cluster to start up.
-    sleep 10;
+    sleep 20;
     python -c "import pygmo; pygmo.test.run_test_suite()"
 elif [[ "${PAGMO_BUILD}" == "OSXPython36" || "${PAGMO_BUILD}" == "OSXPython27" ]]; then
     CXX=clang++ CC=clang cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_WITH_EIGEN3=yes -DPAGMO_INSTALL_HEADERS=no -DPAGMO_BUILD_PYGMO=yes -DCMAKE_CXX_FLAGS="-g0 -O2" ../;
     make install VERBOSE=1;
     ipcluster start --daemonize=True;
     # Give some time for the cluster to start up.
-    sleep 10;
+    sleep 20;
     python -c "import pygmo; pygmo.test.run_test_suite()"
 fi
 
