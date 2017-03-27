6b161f437bec38889b454155c7fdca6872371f02
Francesco Biscani <bluescarni@gmail.com>
2017-03-27 18:50:36 +0200

    Small change.
    [skip appveyor]

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -44,6 +44,8 @@ elif [[ "${PAGMO_BUILD}" == "PygmoPython35" ]]; then
     CXX=g++-4.8 CC=gcc-4.8 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_WITH_EIGEN3=yes -DPAGMO_INSTALL_HEADERS=no -DPAGMO_BUILD_PYGMO=yes ../;
     make install VERBOSE=1;
     ipcluster start --daemonize=True;
+    # Give some time for the cluster to start up.
+    sleep 10;
     python -c "import pygmo; pygmo.test.run_test_suite()"
 fi
 
