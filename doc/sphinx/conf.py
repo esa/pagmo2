245f2939c38fc57c89a6b8fd4d7b032a42f9496e
Dario Izzo <dario.izzo@gmail.com>
2017-04-18 19:08:38 +0200

    new css and font

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -223,7 +223,7 @@ html_favicon = 'docs/images/logo_favico.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
