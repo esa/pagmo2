736253d0b5471b81c91b7d566ed8bbfcba54c659
Francesco Biscani <bluescarni@gmail.com>
2017-05-23 00:46:57 +0200

    Attempt fix for bootstrap theme breakages.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -56,7 +56,7 @@ elif [[ "${PAGMO_BUILD}" == Python* ]]; then
     python travis_additional_tests.py
     cd ../build
     # At the moment conda has these packages only for Python 3.4. Install via pip instead.
-    pip install 'sphinx<1.6' breathe requests[security] sphinx-bootstrap-theme;
+    pip install 'sphinx<1.6' breathe requests[security] 'sphinx-bootstrap-theme<0.5';
     # Run doxygen and check the output.
     cd ../doc/doxygen;
     export DOXYGEN_OUTPUT=`doxygen 2>&1 >/dev/null`;
