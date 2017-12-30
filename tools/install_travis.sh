3c294de942c167037ba67ffae83eb2519d3498b0
Francesco Biscani <bluescarni@gmail.com>
2017-12-30 02:04:07 +0100

    Revert "Try installing cloudpickle from pip as a workaround for the CI failure."
    This reverts commit 7f7f9a10b6fa9966e421b6e77a19ab71990ce436.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -48,7 +48,6 @@ elif [[ "${PAGMO_BUILD}" == "OSXRelease" ]]; then
 elif [[ "${PAGMO_BUILD}" == Python* ]]; then
     export CXX=g++-4.8
     export CC=gcc-4.8
-    pip install cloudpickle
     # Install pagmo first.
     cd ..;
     mkdir build_pagmo;
@@ -153,7 +152,6 @@ elif [[ "${PAGMO_BUILD}" == Python* ]]; then
 elif [[ "${PAGMO_BUILD}" == OSXPython* ]]; then
     export CXX=clang++
     export CC=clang
-    pip install cloudpickle
     # Install pagmo first.
     cd ..;
     mkdir build_pagmo;
