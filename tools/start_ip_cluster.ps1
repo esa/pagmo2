54e31d35e38f11aa07d954715b12cc05781085dc
Francesco Biscani <bluescarni@gmail.com>
2018-07-03 23:01:21 +0200

    ..

--- tools/start_ip_cluster.ps1
+++ tools/start_ip_cluster.ps1
@@ -1,1 +1,4 @@
-Start-Process "ipcluster" -ArgumentList "start"
+# Small script to start ipcluster as a background process.
+
+# Start-Process "ipcluster" -ArgumentList "start"
+Start-Job -ScriptBlock {ipcluster start}
