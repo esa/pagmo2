61197c9d3e0eb3659ecbb75386759715f9b15b5b
Francesco Biscani <bluescarni@gmail.com>
2018-07-20 23:35:38 +0200

    Evolving to a decorator problem.

--- pygmo/_py_problems.py
+++ pygmo/_py_problems.py
@@ -32,22 +32,28 @@
 from __future__ import absolute_import as _ai
 
 
-def _with_callback(f):
+def _with_decorator(f):
     from functools import wraps
 
     @wraps(f)
     def wrapper(self, *args, **kwds):
-        cb = self._callbacks.get(f.__name__)
-        if not cb is None:
-            cb(self, *args, **kwds)
-        return f(self, *args, **kwds)
+        dec = self._decors.get(f.__name__)
+        if dec is None:
+            return f(self, *args, **kwds)
+        else:
+            return dec(f)(self, *args, **kwds)
     return wrapper
 
 
-class problem_callback(object):
+class decorator_problem(object):
+    """Decorator meta-problem.
+
+
+    """
+
     def __init__(self, prob, **kwargs):
+        from . import problem
         from warnings import warn
-        from pygmo import problem
         from copy import deepcopy
         if type(prob) == problem:
             # If prob is a pygmo problem, we will make a copy
@@ -60,95 +66,95 @@ class problem_callback(object):
             # Otherwise, we attempt to create a problem from it. This will
             # work if prob is an exposed C++ problem or a Python UDP.
             self._prob = problem(prob)
-        self._callbacks = {}
+        self._decors = {}
         for k in kwargs:
-            if k.endswith("_callback"):
+            if k.endswith("_decorator"):
                 if not callable(kwargs[k]):
                     raise ValueError(
-                        "Cannot register the callback for the '{}' method: the supplied object "
-                        "'{}' is not callable".format(k[:-9], kwargs[k]))
-                self._callbacks[k[:-9]] = kwargs[k]
+                        "Cannot register the decorator for the '{}' method: the supplied object "
+                        "'{}' is not callable".format(k[:-10], kwargs[k]))
+                self._decors[k[:-10]] = deepcopy(kwargs[k])
             else:
                 warn("Unrecognized keyword argument: '{}'".format(k))
 
-    @_with_callback
+    @_with_decorator
     def fitness(self, dv):
         return self._prob.fitness(dv)
 
-    @_with_callback
+    @_with_decorator
     def get_bounds(self):
         return self._prob.get_bounds()
 
-    @_with_callback
+    @_with_decorator
     def get_nobj(self):
         return self._prob.get_nobj()
 
-    @_with_callback
+    @_with_decorator
     def get_nec(self):
         return self._prob.get_nec()
 
-    @_with_callback
+    @_with_decorator
     def get_nic(self):
         return self._prob.get_nic()
 
-    @_with_callback
+    @_with_decorator
     def get_nix(self):
         return self._prob.get_nix()
 
-    @_with_callback
+    @_with_decorator
     def has_gradient(self):
         return self._prob.has_gradient()
 
-    @_with_callback
+    @_with_decorator
     def gradient(self, dv):
         return self._prob.gradient(dv)
 
-    @_with_callback
+    @_with_decorator
     def has_gradient_sparsity(self):
         return self._prob.has_gradient_sparsity()
 
-    @_with_callback
+    @_with_decorator
     def gradient_sparsity(self):
         return self._prob.gradient_sparsity()
 
-    @_with_callback
+    @_with_decorator
     def has_hessians(self):
         return self._prob.has_hessians()
 
-    @_with_callback
+    @_with_decorator
     def hessians(self, dv):
         return self._prob.hessians(dv)
 
-    @_with_callback
+    @_with_decorator
     def has_hessians_sparsity(self):
         return self._prob.has_hessians_sparsity()
 
-    @_with_callback
+    @_with_decorator
     def hessians_sparsity(self):
         return self._prob.hessians_sparsity()
 
-    @_with_callback
+    @_with_decorator
     def has_set_seed(self):
         return self._prob.has_set_seed()
 
-    @_with_callback
+    @_with_decorator
     def set_seed(self, s):
         return self._prob.set_seed(s)
 
-    @_with_callback
+    @_with_decorator
     def get_name(self):
-        return self._prob.get_name() + " [with callbacks]"
+        return self._prob.get_name() + " [decorated]"
 
-    @_with_callback
+    @_with_decorator
     def get_extra_info(self):
         retval = self._prob.get_extra_info()
-        if len(self._callbacks) == 0:
-            retval += "\tNo registered callbacks.\n"
+        if len(self._decors) == 0:
+            retval += "\tNo registered decorators.\n"
         else:
-            retval += "\tRegistered callbacks:\n"
-            for i, k in enumerate(self._callbacks):
+            retval += "\tRegistered decorators:\n"
+            for i, k in enumerate(self._decors):
                 retval += "\t\t" + k + \
-                    (",\n" if i < len(self._callbacks) - 1 else "")
+                    (",\n" if i < len(self._decors) - 1 else "")
             retval += '\n'
         return retval
 
