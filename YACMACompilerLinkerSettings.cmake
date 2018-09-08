d868d249954333afdaf03e4b3735ddf1b4654018
Francesco Biscani <bluescarni@gmail.com>
2018-09-08 02:11:10 +0200

    Squashed 'cmake_modules/yacma/' changes from ec1c5e8f..37800d46
    37800d46 Merge commit 'f56ff8b35561ea8e8c6fd5b736a0f07399084423' into pr/yacma_iter
    4613a9d5 Merge commit '8e8c01bc394421c65b117974d117b0ae05ddb927' into pr/small_bits
    79373f8a Merge commit '28bfc37d274df5787a762d4a56f2a93a6230e1db' into pr/build_fixes
    44b975ae Various doc fixes involving metas.
    
    git-subtree-dir: cmake_modules/yacma
    git-subtree-split: 37800d46ad0edb33237dbac1f037c425ed799704

--- YACMACompilerLinkerSettings.cmake
+++ YACMACompilerLinkerSettings.cmake
@@ -134,7 +134,7 @@ if(NOT _YACMACompilerLinkerSettingsRun)
         _YACMA_CHECK_ENABLE_DEBUG_CXX_FLAG(-Wduplicated-cond)
         _YACMA_CHECK_ENABLE_DEBUG_CXX_FLAG(-Wnull-dereference)
         # From GCC 7.
-        _YACMA_CHECK_ENABLE_DEBUG_CXX_FLAG(-Wduplicated-branches)
+        #_YACMA_CHECK_ENABLE_DEBUG_CXX_FLAG(-Wduplicated-branches)
         _YACMA_CHECK_ENABLE_DEBUG_CXX_FLAG(-Wrestrict)
         _YACMA_CHECK_ENABLE_DEBUG_CXX_FLAG(-Waligned-new)
         # From GCC 8.
