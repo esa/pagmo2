eb7f4f9dbffd8f2d03b243aba0aec214fda9befd
Francesco Biscani <bluescarni@gmail.com>
2018-11-09 15:23:28 +0100

    Initial code for the selection of the serialization backend.

--- pygmo/__init__.py
+++ pygmo/__init__.py
@@ -621,6 +621,81 @@ import atexit as _atexit
 from .core import _cleanup as _cpp_cleanup
 
 
+# Machinery for the setup of the serialization backend.
+import cloudpickle as _cloudpickle
+_serialization_backend = _cloudpickle
+
+
+def set_serialization_backend(name):
+    """Set pygmo's serialization backend.
+
+    This function allows to specify the serialization backend that is used internally by pygmo
+    for the (de)serialization of pythonic user-defined entities (e.g., user-defined pythonic
+    problems, algorithms, etc.).
+
+    By default, pygmo uses the `cloudpickle <https://github.com/cloudpipe/cloudpickle>`__
+    module, which extends the capabilities of the standard :mod:`pickle` module with support
+    for lambdas, functions and classes defined interactively in the ``__main__`` module, etc.
+
+    In some specific cases, however, different serialization backends might work better than cloudpickle,
+    and thus pygmo provides the possibility for the cognizant user to switch to another
+    serialization backend.
+
+    The valid backends are:
+
+    * ``'pickle'`` (i.e., the standard Python :mod:`pickle` module),
+    * ``'cloudpickle'``,
+    * ``'dill'`` (from the `dill <https://pypi.org/project/dill/>`__ library).
+
+    .. warning::
+
+       Setting the serialization backend is not thread-safe: do **not** set
+       the serialization backend while concurrently setting/getting it from another thread.
+
+    Args:
+        name (str): the name of the desired backend
+
+    Raises:
+        TypeError: if *name* is not a :class:`str`
+        ValueError: if *name* is not one of ``['pickle', 'cloudpickle', 'dill']``
+        ImportError: if *name* is ``'dill'`` but the dill module is not installed
+
+    """
+    if not isinstance(name, str):
+        raise TypeError(
+            "The serialization backend must be specified as a string, but an object of type {} was provided instead".format(type(name)))
+    global _serialization_backend
+    if name == "pickle":
+        import pickle
+        _serialization_backend = pickle
+    elif name == "cloudpickle":
+        _serialization_backend = _cloudpickle
+    elif name == "dill":
+        try:
+            import dill
+            _serialization_backend = dill
+        except ImportError:
+            raise ImportError(
+                "The 'dill' serialization backend was specified, but the dill module is not installed.")
+    else:
+        raise ValueError(
+            "The serialization backend '{}' is not valid. The valid backends are: ['pickle', 'cloudpickle', 'dill']".format(name))
+
+
+def get_serialization_backend():
+    """Get pygmo's serialization backend.
+
+    This function will return pygmo's current serialization backend (see
+    :func:`~pygmo.set_serialization_backend()` for an explanation of the
+    available backends).
+
+    Returns:
+       types.ModuleType: the current serialization backend (as a Python module)
+
+    """
+    return _serialization_backend
+
+
 def _cleanup():
     mp_island.shutdown_pool()
     _cpp_cleanup()
