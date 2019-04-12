9d0c7e1d28d39d33dee8d63d7a4929bcc65a2949
Francesco Biscani <bluescarni@gmail.com>
2019-04-12 23:57:47 +0200

    Missing dep.

--- tools/circleci_bionic_gcc7_coverage.sh
+++ tools/circleci_bionic_gcc7_coverage.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential cmake libboost-dev libnlopt-dev libeigen3-dev coinor-libipopt-dev
+sudo apt-get install build-essential cmake libboost-dev libnlopt-dev libeigen3-dev coinor-libipopt-dev curl
 
 # Create the build dir and cd into it.
 mkdir build
