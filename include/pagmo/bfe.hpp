1b2bb561c56cb2d0615e800c830b263542d279e3
Francesco Biscani <bluescarni@gmail.com>
2019-04-25 21:43:58 +0200

    More WIP.

--- include/pagmo/bfe.hpp
+++ include/pagmo/bfe.hpp
@@ -29,34 +29,29 @@ see https://www.gnu.org/licenses/. */
 #ifndef PAGMO_BFE_HPP
 #define PAGMO_BFE_HPP
 
-#include <algorithm>
 #include <cassert>
 #include <functional>
 #include <iostream>
-#include <iterator>
-#include <limits>
 #include <memory>
-#include <stdexcept>
 #include <string>
 #include <type_traits>
 #include <typeinfo>
 #include <utility>
 
-#include <boost/numeric/conversion/cast.hpp>
-
-#include <tbb/blocked_range.h>
-#include <tbb/parallel_for.h>
-
-#include <pagmo/detail/bfe_impl.hpp>
 #include <pagmo/detail/make_unique.hpp>
-#include <pagmo/exceptions.hpp>
+#include <pagmo/detail/visibility.hpp>
 #include <pagmo/problem.hpp>
-#include <pagmo/serialization.hpp>
+#include <pagmo/s11n.hpp>
 #include <pagmo/threading.hpp>
 #include <pagmo/type_traits.hpp>
 #include <pagmo/types.hpp>
 
-#define PAGMO_REGISTER_BFE(b) CEREAL_REGISTER_TYPE_WITH_NAME(pagmo::detail::bfe_inner<b>, "udbfe " #b)
+#define PAGMO_S11N_BFE_EXPORT_KEY(b) BOOST_CLASS_EXPORT_KEY2(pagmo::detail::bfe_inner<b>, "udbfe " #b)
+#define PAGMO_S11N_BFE_IMPLEMENT(b) BOOST_CLASS_EXPORT_IMPLEMENT(pagmo::detail::bfe_inner<b>)
+
+#define PAGMO_S11N_BFE_EXPORT(b)                                                                                       \
+    PAGMO_S11N_BFE_EXPORT_KEY(b)                                                                                       \
+    PAGMO_S11N_BFE_IMPLEMENT(b)
 
 namespace pagmo
 {
@@ -118,7 +113,7 @@ struct bfe_inner_base {
     virtual std::string get_extra_info() const = 0;
     virtual thread_safety get_thread_safety() const = 0;
     template <typename Archive>
-    void serialize(Archive &)
+    void serialize(Archive &, unsigned)
     {
     }
 };
@@ -190,9 +185,9 @@ struct bfe_inner final : bfe_inner_base {
     }
     // Serialization.
     template <typename Archive>
-    void serialize(Archive &ar)
+    void serialize(Archive &ar, unsigned)
     {
-        ar(cereal::base_class<bfe_inner_base>(this), m_value);
+        detail::archive(ar, boost::serialization::base_object<bfe_inner_base>(*this), m_value);
     }
     T m_value;
 };
@@ -200,94 +195,11 @@ struct bfe_inner final : bfe_inner_base {
 } // namespace detail
 
 // Multi-threaded bfe.
-class thread_bfe
+class PAGMO_PUBLIC thread_bfe
 {
 public:
     // Call operator.
-    vector_double operator()(const problem &p, const vector_double &dvs) const
-    {
-        // Fetch a few quantities from the problem.
-        // Problem dimension.
-        const auto n_dim = p.get_nx();
-        // Fitness dimension.
-        const auto f_dim = p.get_nf();
-        // Total number of dvs.
-        const auto n_dvs = dvs.size() / n_dim;
-
-        // NOTE: as usual, we assume that thread_bfe is always wrapped
-        // by a bfe, where we already check that dvs
-        // is compatible with p.
-        // NOTE: this is what we always do with user-defined classes:
-        // we do the sanity checks in the type-erased container.
-        assert(dvs.size() % n_dim == 0u);
-
-        // Prepare the return value.
-        // Guard against overflow.
-        // LCOV_EXCL_START
-        if (n_dvs > std::numeric_limits<vector_double::size_type>::max() / f_dim) {
-            pagmo_throw(std::overflow_error,
-                        "Overflow detected in the computation of the size of the output of a thread_bfe");
-        }
-        // LCOV_EXCL_STOP
-        vector_double retval(n_dvs * f_dim);
-
-        // Functor to implement the fitness evaluation of a range of input dvs. begin/end are the indices
-        // of the individuals in dv (ranging from 0 to n_dvs), the resulting fitnesses will be written directly into
-        // retval.
-        auto range_evaluator = [&dvs, &retval, n_dim, f_dim, n_dvs](const problem &prob, decltype(dvs.size()) begin,
-                                                                    decltype(dvs.size()) end) {
-            assert(begin <= end);
-            assert(end <= n_dvs);
-            (void)n_dvs;
-
-            // Temporary dv that will be used for fitness evaluation.
-            vector_double tmp_dv(n_dim);
-            for (; begin != end; ++begin) {
-                auto in_ptr = dvs.data() + begin * n_dim;
-                auto out_ptr = retval.data() + begin * f_dim;
-                std::copy(
-#if defined(_MSC_VER)
-                    stdext::make_checked_array_iterator(in_ptr, n_dim),
-                    stdext::make_checked_array_iterator(in_ptr, n_dim, n_dim), tmp_dv.begin()
-#else
-                    in_ptr, in_ptr + n_dim, tmp_dv.begin()
-#endif
-                );
-                const auto fv = prob.fitness(tmp_dv);
-                assert(fv.size() == f_dim);
-                std::copy(
-#if defined(_MSC_VER)
-                    fv.begin(), fv.end(), stdext::make_checked_array_iterator(out_ptr, f_dim)
-#else
-                    fv.begin(), fv.end(), out_ptr
-#endif
-                );
-            }
-        };
-
-        using range_t = tbb::blocked_range<decltype(dvs.size())>;
-        if (p.get_thread_safety() >= thread_safety::constant) {
-            // We can concurrently call the objfun on the input prob, hence we can
-            // capture it by reference and do all the fitness calls on the same object.
-            tbb::parallel_for(range_t(0u, n_dvs), [&p, &range_evaluator](const range_t &range) {
-                range_evaluator(p, range.begin(), range.end());
-            });
-        } else if (p.get_thread_safety() == thread_safety::basic) {
-            // We cannot concurrently call the objfun on the input prob. We will need
-            // to make a copy of p for each parallel iteration.
-            tbb::parallel_for(range_t(0u, n_dvs), [p, &range_evaluator](const range_t &range) {
-                range_evaluator(p, range.begin(), range.end());
-            });
-            // Manually increment the fitness eval counter in p. Since we used copies
-            // of p for the parallel fitness evaluations, the counter in p did not change.
-            p.increment_fevals(boost::numeric_cast<unsigned long long>(n_dvs));
-        } else {
-            pagmo_throw(std::invalid_argument, "Cannot use a thread_bfe on the problem '" + p.get_name()
-                                                   + "', which does not provide the required level of thread safety");
-        }
-
-        return retval;
-    }
+    vector_double operator()(const problem &, const vector_double &) const;
     // Name.
     std::string get_name() const
     {
@@ -295,20 +207,17 @@ public:
     }
     // Serialization support.
     template <typename Archive>
-    void serialize(Archive &)
+    void serialize(Archive &, unsigned)
     {
     }
 };
 
 // Bfe that uses problem's member function.
-class member_bfe
+class PAGMO_PUBLIC member_bfe
 {
 public:
     // Call operator.
-    vector_double operator()(const problem &p, const vector_double &dvs) const
-    {
-        return detail::prob_invoke_mem_batch_fitness(p, dvs);
-    }
+    vector_double operator()(const problem &, const vector_double &) const;
     // Name.
     std::string get_name() const
     {
@@ -316,7 +225,7 @@ public:
     }
     // Serialization support.
     template <typename Archive>
-    void serialize(Archive &)
+    void serialize(Archive &, unsigned)
     {
     }
 };
@@ -324,46 +233,17 @@ public:
 namespace detail
 {
 
-// Usual trick for global variables in header file.
-template <typename = void>
-struct default_bfe_impl {
-    static std::function<vector_double(const problem &, const vector_double &)> s_func;
-};
-
-// C++ implementation of the heuristic for the automatic deduction of the "best"
-// bfe strategy.
-inline vector_double default_bfe_cpp_impl(const problem &p, const vector_double &dvs)
-{
-    // The member function batch_fitness() of p, if present, has priority.
-    if (p.has_batch_fitness()) {
-        return member_bfe{}(p, dvs);
-    }
-    // Otherwise, we run the generic thread-based bfe, if the problem
-    // is thread-safe enough.
-    if (p.get_thread_safety() >= thread_safety::basic) {
-        return thread_bfe{}(p, dvs);
-    }
-    pagmo_throw(std::invalid_argument,
-                "Cannot execute fitness evaluations in batch mode for a problem of type '" + p.get_name()
-                    + "': the problem does not implement the batch_fitness() member function, and its thread safety "
-                      "level is not sufficient to run a thread-based batch fitness evaluation implementation");
-}
-
-template <typename T>
-std::function<vector_double(const problem &, const vector_double &)> default_bfe_impl<T>::s_func
-    = &default_bfe_cpp_impl;
+// Helper for the selection of the default bfe implementation.
+PAGMO_PUBLIC extern std::function<vector_double(const problem &, const vector_double &)> default_bfe_impl;
 
 } // namespace detail
 
 // Default bfe implementation.
-class default_bfe
+class PAGMO_PUBLIC default_bfe
 {
 public:
     // Call operator.
-    vector_double operator()(const problem &p, const vector_double &dvs) const
-    {
-        return detail::default_bfe_impl<>::s_func(p, dvs);
-    }
+    vector_double operator()(const problem &, const vector_double &) const;
     // Name.
     std::string get_name() const
     {
@@ -371,12 +251,12 @@ public:
     }
     // Serialization support.
     template <typename Archive>
-    void serialize(Archive &)
+    void serialize(Archive &, unsigned)
     {
     }
 };
 
-class bfe
+class PAGMO_PUBLIC bfe
 {
     // Enable the generic ctor only if T is not a bfe (after removing
     // const/reference qualifiers), and if T is a udbfe. Additionally,
@@ -402,39 +282,26 @@ class bfe
         : m_ptr(detail::make_unique<detail::bfe_inner<uncvref_t<T>>>(std::forward<T>(x)))
     {
     }
+    // Implementation of the generic ctor.
+    void generic_ctor_impl();
 
 public:
     // Default ctor.
-    bfe() : bfe(default_bfe{}) {}
+    bfe();
     // Constructor from a UDBFE.
     template <typename T, generic_ctor_enabler<T> = 0>
     explicit bfe(T &&x) : bfe(std::forward<T>(x), std::is_function<uncvref_t<T>>{})
     {
-        // Assign the name.
-        m_name = ptr()->get_name();
-        // Assign the thread safety level.
-        m_thread_safety = ptr()->get_thread_safety();
+        generic_ctor_impl();
     }
     // Copy constructor.
-    bfe(const bfe &other) : m_ptr(other.ptr()->clone()), m_name(other.m_name), m_thread_safety(other.m_thread_safety) {}
-    // Move constructor. The default implementation is fine.
-    bfe(bfe &&) noexcept = default;
+    bfe(const bfe &);
+    // Move constructor.
+    bfe(bfe &&) noexcept;
     // Move assignment operator
-    bfe &operator=(bfe &&other) noexcept
-    {
-        if (this != &other) {
-            m_ptr = std::move(other.m_ptr);
-            m_name = std::move(other.m_name);
-            m_thread_safety = std::move(other.m_thread_safety);
-        }
-        return *this;
-    }
+    bfe &operator=(bfe &&) noexcept;
     // Copy assignment operator
-    bfe &operator=(const bfe &other)
-    {
-        // Copy ctor + move assignment.
-        return *this = bfe(other);
-    }
+    bfe &operator=(const bfe &);
     // Extraction and related.
     template <typename T>
     const T *extract() const noexcept
@@ -454,26 +321,14 @@ public:
         return extract<T>() != nullptr;
     }
     // Call operator.
-    vector_double operator()(const problem &p, const vector_double &dvs) const
-    {
-        // Check the input dvs.
-        detail::bfe_check_input_dvs(p, dvs);
-        // Invoke the call operator from the UDBFE.
-        auto retval((*ptr())(p, dvs));
-        // Check the produced vector of fitnesses.
-        detail::bfe_check_output_fvs(p, dvs, retval);
-        return retval;
-    }
+    vector_double operator()(const problem &, const vector_double &) const;
     // Name.
     std::string get_name() const
     {
         return m_name;
     }
     // Extra info.
-    std::string get_extra_info() const
-    {
-        return ptr()->get_extra_info();
-    }
+    std::string get_extra_info() const;
     // Thread safety level.
     thread_safety get_thread_safety() const
     {
@@ -481,29 +336,19 @@ public:
     }
     // Serialisation support.
     template <typename Archive>
-    void save(Archive &ar) const
+    void save(Archive &ar, unsigned) const
     {
-        ar(m_ptr, m_name, m_thread_safety);
+        detail::to_archive(ar, m_ptr, m_name, m_thread_safety);
     }
     template <typename Archive>
-    void load(Archive &ar)
+    void load(Archive &ar, unsigned)
     {
         // Deserialize in a separate object and move it in later, for exception safety.
         bfe tmp_bfe;
-        ar(tmp_bfe.m_ptr, tmp_bfe.m_name, tmp_bfe.m_thread_safety);
+        detail::from_archive(ar, tmp_bfe.m_ptr, tmp_bfe.m_name, tmp_bfe.m_thread_safety);
         *this = std::move(tmp_bfe);
     }
-    // Stream operator.
-    friend std::ostream &operator<<(std::ostream &os, const bfe &b)
-    {
-        os << "BFE name: " << b.get_name() << '\n';
-        os << "\n\tThread safety: " << b.get_thread_safety() << '\n';
-        const auto extra_str = b.get_extra_info();
-        if (!extra_str.empty()) {
-            os << "\nExtra info:\n" << extra_str << '\n';
-        }
-        return os;
-    }
+    BOOST_SERIALIZATION_SPLIT_MEMBER()
 
 private:
     // Just two small helpers to make sure that whenever we require
@@ -531,10 +376,13 @@ private:
     thread_safety m_thread_safety;
 };
 
+// Stream operator.
+PAGMO_PUBLIC std::ostream &operator<<(std::ostream &, const bfe &);
+
 } // namespace pagmo
 
-PAGMO_REGISTER_BFE(pagmo::thread_bfe)
-PAGMO_REGISTER_BFE(pagmo::member_bfe)
-PAGMO_REGISTER_BFE(pagmo::default_bfe)
+PAGMO_S11N_BFE_EXPORT_KEY(pagmo::thread_bfe)
+PAGMO_S11N_BFE_EXPORT_KEY(pagmo::member_bfe)
+PAGMO_S11N_BFE_EXPORT_KEY(pagmo::default_bfe)
 
 #endif
--- tests/bfe.cpp
+++ tests/bfe.cpp
@@ -49,7 +49,7 @@ see https://www.gnu.org/licenses/. */
 #include <pagmo/bfe.hpp>
 #include <pagmo/problem.hpp>
 #include <pagmo/problems/rosenbrock.hpp>
-#include <pagmo/serialization.hpp>
+#include <pagmo/s11n.hpp>
 #include <pagmo/threading.hpp>
 #include <pagmo/types.hpp>
 
@@ -213,15 +213,15 @@ BOOST_AUTO_TEST_CASE(basic_tests)
         std::stringstream ss;
         {
             before = boost::lexical_cast<std::string>(bfe0);
-            cereal::JSONOutputArchive oarchive(ss);
-            oarchive(bfe0);
+            boost::archive::text_oarchive oarchive(ss);
+            oarchive << bfe0;
         }
         bfe0 = bfe{udbfe0};
         BOOST_CHECK(bfe0.is<udbfe_func_t>());
         BOOST_CHECK(before != boost::lexical_cast<std::string>(bfe0));
         {
-            cereal::JSONInputArchive iarchive(ss);
-            iarchive(bfe0);
+            boost::archive::text_iarchive iarchive(ss);
+            iarchive >> bfe0;
         }
         BOOST_CHECK(before == boost::lexical_cast<std::string>(bfe0));
         BOOST_CHECK(bfe0.is<default_bfe>());
@@ -410,14 +410,14 @@ struct udbfe_a {
         return thread_safety::constant;
     }
     template <typename Archive>
-    void serialize(Archive &ar)
+    void serialize(Archive &ar, unsigned)
     {
         ar &state;
     }
     int state = 42;
 };
 
-PAGMO_REGISTER_BFE(udbfe_a)
+PAGMO_S11N_BFE_EXPORT(udbfe_a)
 
 // Serialization tests.
 BOOST_AUTO_TEST_CASE(s11n)
@@ -430,14 +430,14 @@ BOOST_AUTO_TEST_CASE(s11n)
     auto before = boost::lexical_cast<std::string>(bfe0);
     // Now serialize, deserialize and compare the result.
     {
-        cereal::JSONOutputArchive oarchive(ss);
-        oarchive(bfe0);
+        boost::archive::text_oarchive oarchive(ss);
+        oarchive << bfe0;
     }
     // Change the content of p before deserializing.
     bfe0 = bfe{};
     {
-        cereal::JSONInputArchive iarchive(ss);
-        iarchive(bfe0);
+        boost::archive::text_iarchive iarchive(ss);
+        iarchive >> bfe0;
     }
     auto after = boost::lexical_cast<std::string>(bfe0);
     BOOST_CHECK_EQUAL(before, after);
