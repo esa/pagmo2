f24fcb843f9a81ea11e1e71df9f6d58f78d0f4eb
Francesco Biscani <bluescarni@gmail.com>
2019-04-28 22:16:53 +0200

    generic utils -> lib

--- include/pagmo/utils/generic.hpp
+++ include/pagmo/utils/generic.hpp
@@ -32,16 +32,15 @@ see https://www.gnu.org/licenses/. */
 #include <cassert>
 #include <cmath>
 #include <limits>
-#include <numeric>
 #include <random>
 #include <stdexcept>
 #include <string>
-#include <utility>
 #include <vector>
 
 #include <boost/numeric/conversion/cast.hpp>
 
 #include <pagmo/detail/custom_comparisons.hpp>
+#include <pagmo/detail/visibility.hpp>
 #include <pagmo/exceptions.hpp>
 #include <pagmo/problem.hpp>
 #include <pagmo/types.hpp>
@@ -53,19 +52,7 @@ namespace detail
 {
 
 // Checks that all elements of the problem bounds are not equal
-inline bool some_bound_is_equal(const problem &prob)
-{
-    // Some variable renaming
-    const auto &lb = prob.get_lb();
-    const auto &ub = prob.get_ub();
-    // Since the bounds are extracted from problem we can be sure they have equal length
-    for (decltype(lb.size()) i = 0u; i < lb.size(); ++i) {
-        if (lb[i] == ub[i]) {
-            return true;
-        }
-    }
-    return false;
-}
+PAGMO_PUBLIC bool some_bound_is_equal(const problem &);
 
 // Check that the lower/upper bounds lb/ub are suitable for the
 // generation of a real number. The boolean flags specify at
@@ -401,86 +388,16 @@ inline vector_double batch_random_decision_vector(const problem &prob, vector_do
     return out;
 }
 
-/// Binomial coefficient
-/**
- * An implementation of the binomial coefficient using gamma functions
- * @param  n first parameter \f$n\f$
- * @param  k second paramater \f$k\f$
- * @return the binomial coefficient \f$ n \choose k \f$
- */
-inline double binomial_coefficient(vector_double::size_type n, vector_double::size_type k)
-{
-    if (k <= n) {
-        return std::round(std::exp(std::lgamma(static_cast<double>(n) + 1.) - std::lgamma(static_cast<double>(k) + 1.)
-                                   - std::lgamma(static_cast<double>(n) - static_cast<double>(k) + 1.)));
-    } else {
-        pagmo_throw(std::invalid_argument, "The binomial coefficient is only defined for k<=n, you requested n="
-                                               + std::to_string(n) + " and k=" + std::to_string(k));
-    }
-}
+// Binomial coefficient
+PAGMO_PUBLIC double binomial_coefficient(vector_double::size_type, vector_double::size_type);
 
-/// K-Nearest Neighbours
-/**
- * Computes the indexes of the k nearest neighbours (euclidean distance) to each of the input points.
- * The algorithm complexity (naive implementation) is \f$ O(MN^2)\f$ where \f$N\f$ is the number of
- * points and \f$M\f$ their dimensionality
- *
- * Example:
- * @code{.unparsed}
- * auto res = kNN({{1, 1}, {2, 2}, {3.1, 3.1}, {5, 5}}, 2u);
- * @endcode
- *
- * @param points the \f$N\f$ points having dimension \f$M\f$
- * @param k number of neighbours to detect
- * @return An <tt>std::vector<std::vector<population::size_type> > </tt> containing the indexes of the k nearest
- * neighbours sorted by distance
- * @throws std::invalid_argument If the points do not all have the same dimension.
- */
-inline std::vector<std::vector<vector_double::size_type>> kNN(const std::vector<vector_double> &points,
-                                                              std::vector<vector_double>::size_type k)
-{
-    std::vector<std::vector<vector_double::size_type>> neigh_idxs;
-    auto N = points.size();
-    if (N == 0u) {
-        return {};
-    }
-    auto M = points[0].size();
-    if (!std::all_of(points.begin(), points.end(), [M](const vector_double &p) { return p.size() == M; })) {
-        pagmo_throw(std::invalid_argument, "All points must have the same dimensionality for k-NN to be invoked");
-    }
-    // loop through the points
-    for (decltype(N) i = 0u; i < N; ++i) {
-        // We compute all the distances to all other points including the self
-        vector_double distances;
-        for (decltype(N) j = 0u; j < N; ++j) {
-            double dist = 0.;
-            for (decltype(M) l = 0u; l < M; ++l) {
-                dist += (points[i][l] - points[j][l]) * (points[i][l] - points[j][l]);
-            }
-            distances.push_back(std::sqrt(dist));
-        }
-        // We sort the indexes with respect to the distance
-        std::vector<vector_double::size_type> idxs(N);
-        std::iota(idxs.begin(), idxs.end(), vector_double::size_type(0u));
-        std::sort(idxs.begin(), idxs.end(), [&distances](vector_double::size_type idx1, vector_double::size_type idx2) {
-            return detail::less_than_f(distances[idx1], distances[idx2]);
-        });
-        // We remove the first element containg the self-distance (0)
-        idxs.erase(std::remove(idxs.begin(), idxs.end(), i), idxs.end());
-        neigh_idxs.push_back(idxs);
-    }
-    // We trim to k the lists if needed
-    if (k < N - 1u) {
-        for (decltype(neigh_idxs.size()) i = 0u; i < neigh_idxs.size(); ++i) {
-            neigh_idxs[i].erase(neigh_idxs[i].begin() + static_cast<int>(k),
-                                neigh_idxs[i].end()); // TODO: remove the static_cast
-        }
-    }
-    return neigh_idxs;
-}
+// K-Nearest Neighbours
+PAGMO_PUBLIC std::vector<std::vector<vector_double::size_type>> kNN(const std::vector<vector_double> &,
+                                                                    std::vector<vector_double>::size_type);
 
 namespace detail
 {
+
 // modifies a chromosome so that it will be in the bounds. elements that are off are resampled at random in the bounds
 template <typename Rng>
 inline void force_bounds_random(vector_double &x, const vector_double &lb, const vector_double &ub, Rng &r_engine)
@@ -493,36 +410,13 @@ inline void force_bounds_random(vector_double &x, const vector_double &lb, const
         }
     }
 }
+
 // modifies a chromosome so that it will be in the bounds. Elements that are off are reflected in the bounds
-inline void force_bounds_reflection(vector_double &x, const vector_double &lb, const vector_double &ub)
-{
-    assert(x.size() == lb.size());
-    assert(x.size() == ub.size());
-    for (decltype(x.size()) j = 0u; j < x.size(); ++j) {
-        while (x[j] < lb[j] || x[j] > ub[j]) {
-            if (x[j] < lb[j]) {
-                x[j] = 2 * lb[j] - x[j];
-            }
-            if (x[j] > ub[j]) {
-                x[j] = 2 * ub[j] - x[j];
-            }
-        }
-    }
-}
+PAGMO_PUBLIC void force_bounds_reflection(vector_double &, const vector_double &, const vector_double &);
+
 // modifies a chromosome so that it will be in the bounds. Elements that are off are set on the bounds
-inline void force_bounds_stick(vector_double &x, const vector_double &lb, const vector_double &ub)
-{
-    assert(x.size() == lb.size());
-    assert(x.size() == ub.size());
-    for (decltype(x.size()) j = 0u; j < x.size(); ++j) {
-        if (x[j] < lb[j]) {
-            x[j] = lb[j];
-        }
-        if (x[j] > ub[j]) {
-            x[j] = ub[j];
-        }
-    }
-}
+PAGMO_PUBLIC void force_bounds_stick(vector_double &, const vector_double &, const vector_double &);
+
 } // namespace detail
 
 } // namespace pagmo
