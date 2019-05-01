59eb5e196bb5201894df3639083e79e7f6f0e35f
Francesco Biscani <bluescarni@gmail.com>
2019-05-01 23:09:55 +0200

    Lennard-Jones.

--- include/pagmo/problems/lennard_jones.hpp
+++ include/pagmo/problems/lennard_jones.hpp
@@ -29,16 +29,11 @@ see https://www.gnu.org/licenses/. */
 #ifndef PAGMO_PROBLEM_LENNARD_JONES_HPP
 #define PAGMO_PROBLEM_LENNARD_JONES_HPP
 
-#include <cmath>
-#include <iostream>
-#include <limits>
-#include <stdexcept>
 #include <string>
 #include <utility>
-#include <vector>
 
-#include <pagmo/exceptions.hpp>
-#include <pagmo/problem.hpp> // needed for cereal registration macro
+#include <pagmo/detail/visibility.hpp>
+#include <pagmo/problem.hpp>
 #include <pagmo/types.hpp>
 
 namespace pagmo
@@ -59,7 +54,7 @@ namespace pagmo
  * See: http://doye.chem.ox.ac.uk/jon/structures/LJ.html
  *
  */
-class lennard_jones
+class PAGMO_PUBLIC lennard_jones
 {
 public:
     /// Constructor from number of atoms
@@ -70,117 +65,27 @@ public:
      *
      * @throw std::invalid_argument if \p atoms is < 3
      */
-    lennard_jones(unsigned atoms = 3u) : m_atoms(atoms)
-    {
-        if (atoms < 3) {
-            pagmo_throw(std::invalid_argument, "The number of atoms in a Lennard Jones Clusters problem must be "
-                                               "positive and greater than 2, while a number of "
-                                                   + std::to_string(atoms) + " was detected.");
-        }
-        if (m_atoms - 2u > std::numeric_limits<unsigned>::max() / 3u) {
-            pagmo_throw(std::overflow_error,
-                        "Overflow caused by the number of atoms requested: " + std::to_string(m_atoms));
-        }
-    }
-    /// Fitness computation
-    /**
-     * Computes the fitness for this UDP
-     *
-     * @param x the decision vector.
-     *
-     * @return the fitness of \p x.
-     */
-    vector_double fitness(const vector_double &x) const
-    {
-        vector_double f(1, 0.);
-        // We evaluate the potential
-        for (unsigned i = 0u; i < (m_atoms - 1u); ++i) {
-            for (unsigned j = (i + 1u); j < m_atoms; ++j) {
-                double sixth, dist;
-                dist = std::pow(_r(i, 0u, x) - _r(j, 0u, x), 2) + std::pow(_r(i, 1u, x) - _r(j, 1u, x), 2)
-                       + std::pow(_r(i, 2u, x) - _r(j, 2u, x), 2); // rij^2
-                if (dist == 0.0) {
-                    f[0] = std::numeric_limits<double>::max();
-                } else {
-                    sixth = std::pow(dist, -3); // rij^-6
-                    f[0] += (std::pow(sixth, 2) - sixth);
-                }
-            }
-        }
-        f[0] = 4 * f[0];
-        return f;
-    }
-    /// Box-bounds
-    /**
-     * Returns the box-bounds for this UDP.
-     *
-     * @return the lower and upper bounds for each of the decision vector components
-     */
-    std::pair<vector_double, vector_double> get_bounds() const
-    {
-        unsigned prob_dim = 3u * m_atoms - 6u;
-        vector_double lb(prob_dim, -3.0);
-        vector_double ub(prob_dim, 3.0);
-        for (unsigned i = 0u; i < 3u * m_atoms - 6u; ++i) {
-            if ((i != 0) && (i % 3) == 0) {
-                lb[i] = 0.0;
-                ub[i] = 6.0;
-            }
-        }
-        return {lb, ub};
-    }
-    /// Problem name
-    /**
-     * Returns the problem name.
-     *
-     * @return a string containing the problem name
-     */
-    std::string get_name() const
-    {
-        return "Lennard Jones Cluster (" + std::to_string(m_atoms) + " atoms)";
-    }
-    /// Object serialization
-    /**
-     * This method will save/load \p this into the archive \p ar.
-     *
-     * @param ar target archive.
-     *
-     * @throws unspecified any exception thrown by the serialization of the UDP and of primitive types.
-     */
+    lennard_jones(unsigned atoms = 3u);
+    // Fitness computation
+    vector_double fitness(const vector_double &) const;
+    // Box-bounds
+    std::pair<vector_double, vector_double> get_bounds() const;
+    // Problem name
+    std::string get_name() const;
+    // Object serialization
     template <typename Archive>
-    void serialize(Archive &ar)
-    {
-        ar(m_atoms);
-    }
+    void serialize(Archive &, unsigned);
 
 private:
-    /// Helper function that transforms the decision vector x in atoms positions r
-    double _r(unsigned atom, unsigned coord, const vector_double &x) const
-    {
-        if (atom == 0u) { // x1,y1,z1 fixed
-            return 0.0;
-        } else if (atom == 1) {
-            if (coord < 2u) { // x2,y2    fixed
-                return 0.0;
-            } else { // z2 is a variable
-                return x[0];
-            }
-        } else if (atom == 2u) {
-            if (coord == 0u) { // x3 fixed
-                return 0.0;
-            } else { // y3 and z3 are variables
-                return x[coord];
-            }
-        } else {
-            return x[3u * (atom - 2u) + coord];
-        }
-    }
-    /// Number of atoms
+    // Helper function that transforms the decision vector x in atoms positions r
+    double _r(unsigned, unsigned, const vector_double &) const;
+
+    // Number of atoms
     unsigned m_atoms;
 };
 
 } // namespace pagmo
 
-PAGMO_REGISTER_PROBLEM(pagmo::lennard_jones)
+PAGMO_S11N_PROBLEM_EXPORT_KEY(pagmo::lennard_jones)
 
 #endif
