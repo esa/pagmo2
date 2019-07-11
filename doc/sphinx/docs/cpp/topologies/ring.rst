d7f3737c7cf651c3c44e997d74ee4f31677f8455
Francesco Biscani <bluescarni@gmail.com>
2019-07-11 09:23:58 +0200

    Ring topology: docs+testing.

--- doc/sphinx/docs/cpp/topologies/ring.rst
+++ doc/sphinx/docs/cpp/topologies/ring.rst
@@ -14,11 +14,30 @@ Ring
    This user-defined topology (UDT) represent a bidirectional ring (that is, a ring
    in which each node connects to both the previous and the following nodes).
 
+   .. cpp:function:: explicit ring(double w)
+
+      Constructor from edge weight.
+
+      New edges created via :cpp:func:`~pagmo::ring::push_back()` will have
+      a weight of *w*.
+
+      :except std\:\:invalid_argument: if *w* is not in the :math:`\left[0, 1\right]` range.
+
+   .. cpp:function:: ring()
+
+      Default constructor.
+
+      Equivalent to the constructor from edge weight with *w* = 1.
+
    .. cpp:function:: void push_back()
 
       Add the next vertex.
 
-      This method is a no-op.
+      :except unspecified: any exception thrown by the public API of :cpp:class:`~pagmo::base_bgl_topology`.
+
+   .. cpp:function:: double get_weight() const
+
+      :return: the weight *w* used when constructing this topology.
 
    .. cpp:function:: std::string get_name() const
 
@@ -26,8 +45,13 @@ Ring
 
       :return: ``"Ring"``.
 
-   .. cpp:function:: template <typename Archive> void serialize(Archive &, unsigned)
+   .. cpp:function:: template <typename Archive> void serialize(Archive &ar, unsigned)
 
-      Serialisation support.
+      This function implements the serialisation of a :cpp:class:`~pagmo::ring`.
+
+      :param ar: the input/output archive.
+
+      :exception unspecified: any exception thrown by the serialisation of a :cpp:class:`~pagmo::base_bgl_topology`
+         or of primitive types.
 
 .. cpp:namespace-pop::
--- tests/ring.cpp
+++ tests/ring.cpp
@@ -30,8 +30,122 @@ see https://www.gnu.org/licenses/. */
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
+#include <algorithm>
+#include <cstddef>
+#include <sstream>
+#include <stdexcept>
+
+#include <boost/algorithm/string/predicate.hpp>
+
+#include <pagmo/s11n.hpp>
 #include <pagmo/topologies/ring.hpp>
 
 using namespace pagmo;
 
-BOOST_AUTO_TEST_CASE(basic_test) {}
+void verify_ring_topology(const ring &r)
+{
+    const auto s = r.num_vertices();
+
+    if (s < 2u) {
+        return;
+    }
+
+    const auto w = r.get_weight();
+
+    for (std::size_t i = 0; i < s; ++i) {
+        const auto conn = r.get_connections(i);
+        if (s > 2u) {
+            BOOST_CHECK(conn.first.size() == 2u);
+            BOOST_CHECK(conn.second.size() == 2u);
+        } else {
+            BOOST_CHECK(conn.first.size() == 1u);
+            BOOST_CHECK(conn.second.size() == 1u);
+        }
+        BOOST_CHECK(std::all_of(conn.second.begin(), conn.second.end(), [w](double x) { return x == w; }));
+
+        const auto next = (i + 1u) % s;
+        const auto prev = (i == 0u) ? (s - 1u) : (i - 1u);
+        BOOST_CHECK(std::find(conn.first.begin(), conn.first.end(), next) != conn.first.end());
+        BOOST_CHECK(std::find(conn.first.begin(), conn.first.end(), prev) != conn.first.end());
+    }
+}
+
+BOOST_AUTO_TEST_CASE(basic_test)
+{
+    ring r0;
+    BOOST_CHECK(r0.get_weight() == 1);
+    BOOST_CHECK(r0.num_vertices() == 0u);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 1u);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 2u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 3u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 4u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 5u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 6u);
+    verify_ring_topology(r0);
+
+    r0 = ring(.5);
+    BOOST_CHECK(r0.get_weight() == .5);
+    BOOST_CHECK(r0.num_vertices() == 0u);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 1u);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 2u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 3u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 4u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 5u);
+    verify_ring_topology(r0);
+
+    r0.push_back();
+    BOOST_CHECK(r0.num_vertices() == 6u);
+    verify_ring_topology(r0);
+
+    BOOST_CHECK(r0.get_name() == "Ring");
+
+    // Minimal serialization test.
+    {
+        std::stringstream ss;
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << r0;
+        }
+        ring r1;
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> r1;
+        }
+        BOOST_CHECK(r1.num_vertices() == 6u);
+        BOOST_CHECK(r1.get_weight() == .5);
+        verify_ring_topology(r1);
+    }
+
+    BOOST_CHECK_EXCEPTION(ring(-2), std::invalid_argument, [](const std::invalid_argument &ia) {
+        return boost::contains(ia.what(), " is not in the [0., 1.] range");
+    });
+}
