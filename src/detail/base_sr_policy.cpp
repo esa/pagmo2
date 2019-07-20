9b72b29e3e1be66a49f77cd8177bf1434b95b680
Francesco Biscani <bluescarni@gmail.com>
2019-07-20 20:38:01 +0200

    Fix MinGW warnings.

--- src/detail/base_sr_policy.cpp
+++ src/detail/base_sr_policy.cpp
@@ -38,6 +38,12 @@ see https://www.gnu.org/licenses/. */
 #include <pagmo/exceptions.hpp>
 #include <pagmo/types.hpp>
 
+// MINGW-specific warnings.
+#if defined(__GNUC__) && defined(__MINGW32__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsuggest-attribute=const"
+#endif
+
 namespace pagmo
 {
 
--- src/topologies/ring.cpp
+++ src/topologies/ring.cpp
@@ -34,6 +34,12 @@ see https://www.gnu.org/licenses/. */
 #include <pagmo/topologies/ring.hpp>
 #include <pagmo/topology.hpp>
 
+// MINGW-specific warnings.
+#if defined(__GNUC__) && defined(__MINGW32__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsuggest-attribute=pure"
+#endif
+
 namespace pagmo
 {
 
