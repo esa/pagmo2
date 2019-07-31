7957abc786317a014a378778bca98d119b665f4f
Francesco Biscani <bluescarni@gmail.com>
2019-07-31 23:05:28 +0200

    Usual MinGW bits.

--- src/archipelago.cpp
+++ src/archipelago.cpp
@@ -52,6 +52,12 @@ see https://www.gnu.org/licenses/. */
 #include <pagmo/topology.hpp>
 #include <pagmo/types.hpp>
 
+// MINGW-specific warnings.
+#if defined(__GNUC__) && defined(__MINGW32__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsuggest-attribute=pure"
+#endif
+
 namespace pagmo
 {
 
