6acd21276cdcec43628eabc1abae391e7ef98152
Francesco Biscani <bluescarni@gmail.com>
2019-12-06 02:21:39 +0100

    Debug.

--- tools/circleci_bionic_gcc7_conda_py38.sh
+++ tools/circleci_bionic_gcc7_conda_py38.sh
@@ -16,7 +16,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 export PATH="$deps_dir/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge --force
-conda_pkgs="cmake eigen nlopt ipopt boost boost-cpp tbb tbb-devel python=3.7 numpy cloudpickle dill numba pip"
+conda_pkgs="cmake eigen nlopt ipopt boost boost-cpp tbb tbb-devel python=3.8 numpy cloudpickle dill numba pip"
 conda create -q -p $deps_dir -y
 source activate $deps_dir
 conda install $conda_pkgs -y
@@ -37,6 +37,15 @@ make -j2 VERBOSE=1 install
 # Run the tests.
 cd /
 python -c "import pygmo; pygmo.test.run_test_suite(1)";
+
+echo "DONE"
+echo "DONE"
+echo "DONE"
+echo "DONE"
+echo "DONE"
+echo "DONE"
+echo "DONE"
+ps aux
 
 set +e
 set +x
