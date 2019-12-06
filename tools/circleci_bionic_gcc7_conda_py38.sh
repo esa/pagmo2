2a500e5670995f750bcc8eb0e88f4cc6d85527f3
Francesco Biscani <bluescarni@gmail.com>
2019-12-06 01:58:44 +0100

    Try 3.7.

--- tools/circleci_bionic_gcc7_conda_py38.sh
+++ tools/circleci_bionic_gcc7_conda_py38.sh
@@ -16,7 +16,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 export PATH="$deps_dir/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge --force
-conda_pkgs="cmake eigen nlopt ipopt boost boost-cpp tbb tbb-devel python=3.8 numpy cloudpickle dill numba pip"
+conda_pkgs="cmake eigen nlopt ipopt boost boost-cpp tbb tbb-devel python=3.7 numpy cloudpickle dill numba pip"
 conda create -q -p $deps_dir -y
 source activate $deps_dir
 conda install $conda_pkgs -y
