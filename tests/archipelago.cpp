7e1c6b363a591c37dbb762ff814fd30fc5fbb489
Francesco Biscani <bluescarni@gmail.com>
2020-02-02 23:40:06 +0100

    MinGW debug.

--- tests/archipelago.cpp
+++ tests/archipelago.cpp
@@ -986,9 +986,10 @@ BOOST_AUTO_TEST_CASE(archipelago_set_migrants_db)
     std::get<2>(new_db[0]).push_back(vector_double(3u));
     a.set_migrants_db(new_db);
     a.evolve(1);
-    BOOST_CHECK_EXCEPTION(a.wait_check(), std::invalid_argument, [](const std::invalid_argument &err) {
-        return boost::contains(err.what(), "' have the expected dimension (50)");
-    });
+    a.wait_check();
+    // BOOST_CHECK_EXCEPTION(a.wait_check(), std::invalid_argument, [](const std::invalid_argument &err) {
+    //     return boost::contains(err.what(), "' have the expected dimension (50)");
+    // });
 
     // db with wrong fv length.
     a = archipelago{ring{}, 10u, nsga2{10}, dtlz{2, 50}, 100u};
