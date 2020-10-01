117d36b3930135cb13af1d7c077439fbd6e25c64
Julius Roeder <j.roeder@uva.nl>
2020-10-01 16:48:55 +0200

    adding bin packing problem example

--- doc/sphinx/docs/cpp/tutorials/cpp_tut_bin_packing.rst
+++ doc/sphinx/docs/cpp/tutorials/cpp_tut_bin_packing.rst
@@ -3,15 +3,126 @@
 Bin Packing
 ===========
 
-The original bin packing problem is ... (**references**)
+In the original bin packing problem (*bpp*) one has to *pack* items of different volume/size into a finite number of containers or bins.
+The aim is to minimze the number of containers that have to be used.  This is a NP-hard problem. 
 
-Since then bin packing has been extended in many different ways: 2D bin packing, 3D bin packing, nD bin packing, ... (**references**)
-
-Problems similar to bin packing can be found in many different domains 
-(computer science, logistic, job shop scheduling etc.).
-
-Thus, it is still a very relevant problem with several new solvers being applied/tested/introduced every year. (**references**)
+Since then bin packing has been extended in many different ways: 2D bin packing, 3D bin packing, packing by weight or cost etc.
+Problems similar to bin packing or application of bpp can be found in many different domains (computer science, logistic, job shop scheduling etc.).
+Thus, it is still a very relevant problem with several new solvers being applied/tested/introduced every year 
+(Feng et al. "*An Enhance Grasshopper Optimization Algorithm to the Bin Packing Problem*"; 
+Abdel-Basset et al. "*An improved nature inspired meta-heuristic algorithm for 1-D bin packing problem*").
 
 Here we consider a 1D version of bin packing, where we have a set of objects that need to be packed into a set of bins. 
-All bins have the same size and the aim is to minimize the number of bins used. The different objects do not have to 
-be of the same size.
+All bins have the same size and the aim is to minimize the number of bins used. The different items can be of different size.
+An example of items are shown in the figure below. The total size of all items is 18.5.
+
+.. image:: ../../images/bin_packing/bin_packing_items.png
+
+These items have to fit into into six containers. Each container is of size 7, i.e. there is enough space for all items. 
+A solution minimizing the number of used bins could look like this:
+
+.. image:: ../../images/bin_packing/bin_packing_solution.png
+
+Other solutions using the same number of bins is also possible.
+For a small example the solution is trivial and can easily be made by hand. 
+Lets see how we can solve this problem using pagmo, so that we can also solve non-trivial versions of this problem.
+ 
+Problem Definition
+------------------
+
+For the bin packing problem we need to know 4 things.
+
+- How many bins are there?
+- How large are the bins?
+- How many items are there?
+- How large are the items?
+
+The bounds and constraints are fairly straightforwards. All items have to be packed into one of the bins, i.e. between 0 and *max_bin_num - 1*.
+The sum of the items in a bin cannot be larger than the size of the bins.  
+
+Just like in the :ref:'First Problem<cpp_tut_first_problem>' we have to implement 4 functions: 
+
+- ``fitness()``
+- ``get_bounds()``
+- ``get_nic()``
+- ``get_nix()``
+
+Lets start with the fitness function. As an input it receives a vector of possible item mappings, i.e. a possible solution.
+For this solution it then computes the fitness, which in this case is the number of bins used. 
+The computed fitness value is then stored in a return vector. 
+
+After the fitness value we need to compute if the constraints are met, i.e. is there enough space left in the bins. 
+For this we loop over all bins and all items and compute the usage of each bin. For each bin we add an entry to the 
+return vector which represents the inequality. Each inequality number has to be smaller or equal to 0. 
+
+.. code-block:: c++
+
+    // Implementation of the objective function and all constraints
+    pagmo::vector_double fitness(const pagmo::vector_double &x_i) const {
+        // variables
+        double num_bins_used = 0; //how many bins are used
+        std::vector<int> bin_used(num_bins, 0);
+
+        // where x_i is the bin in which an item i is placed
+        // determine if a bin is used based on x_i
+        for (int i = 0; i < num_items; ++i) {
+            for (int j = 0; j < num_bins; ++j) {
+                if (x_i[i] == j){
+                    bin_used[j] = 1;
+                }
+            }
+        }
+
+        // Objective function calculation; i.e. how many bins are used? We want to minimize this!
+        for(auto& y: bin_used){
+            num_bins_used += y;
+        }
+
+        // Make return vector and adding objective
+        pagmo::vector_double return_vector{num_bins_used};
+
+        // Adding inequalities to stick to the maximum size of the bin
+        for (int j = 0; j < num_bins; ++j) {
+            int temp_bin_occupation = 0;
+            for (int i = 0; i < num_items; ++i) {
+                if (x_i[i] == j){
+                    temp_bin_occupation += items[i] ; // <= 0
+                }
+            }
+            return_vector.emplace_back(temp_bin_occupation - size_bins);
+        }
+        return return_vector;
+    }
+    
+Besides the fitness and the constraints we need to make sure that the bounds are also correct. We do not want that the 
+genetic algorithm starts packing an item into a non existent bin. Therefore, the ``get_bounds()`` function returns the 
+lower and upper bound for each item. In our case the lower bound is 0 (i.e. Bin 0)
+
+.. code-block:: c++
+
+    // The lower and upper bounds are the bins in which an item can be placed.
+    std::pair<pagmo::vector_double, pagmo::vector_double> get_bounds() const {
+        std::vector<double> lower(num_items, 0);
+        std::vector<double> upper(num_items, num_bins-1);
+        return {lower, upper};
+    }
+    
+.. code-block:: c++
+    
+    // Number of inequality constraints.
+    pagmo::vector_double::size_type get_nic() const {
+        return pagmo::vector_double::size_type(num_bins);
+    }
+
+.. code-block:: c++
+
+    // Integer dimension of the problem - i.e. "the chromosomes, where each item is packed"
+    pagmo::vector_double::size_type get_nix() const {
+        return pagmo::vector_double::size_type(num_items);
+    }
+
+
+.. literalinclude:: ../../../../../tutorials/problem_bin_packing.cpp
+   :caption: first_udp_ver0.cpp
+   :language: c++
+   :linenos:
--- tutorials/problem_bin_packing.cpp
+++ tutorials/problem_bin_packing.cpp
@@ -1,191 +1,122 @@
-#include <cmath>
-#include <initializer_list>
 #include <iostream>
 #include <utility>
 
 #include <pagmo/problem.hpp>
 #include <pagmo/types.hpp>
-#include <pagmo/algorithm.hpp>
-#include <pagmo/population.hpp>
 #include <pagmo/algorithms/gaco.hpp>
 #include <pagmo/archipelago.hpp>
 #include <pagmo/topologies/fully_connected.hpp>
 #include <pagmo/algorithms/ihs.hpp>
 
 struct bin_packing {
-    // Number of equality constraints.
-    pagmo::vector_double::size_type get_nec() const{
-        return 10;
-    }
+    // constants of the bin packing problem (bpp)
+    int size_bins; // size and number of available bins
+    int num_bins;
+    std::vector<int> items; // items that have to be packed
+    int num_items = int(items.size());
 
     // Number of inequality constraints.
-    pagmo::vector_double::size_type get_nic() const{
-        return 10;
+    pagmo::vector_double::size_type get_nic() const {
+        return pagmo::vector_double::size_type(num_bins);
     }
 
-    // Number of
-    pagmo::vector_double::size_type get_nix() const
-    {
-        return 80;
+    // Integer dimension of the problem - i.e. "the chromosomes, where each item is packed"
+    pagmo::vector_double::size_type get_nix() const {
+        return pagmo::vector_double::size_type(num_items);
     }
 
     // Implementation of the objective function and all constraints
-    pagmo::vector_double fitness(const pagmo::vector_double &x_ij) const{
-        //constants
-        int B = 30; //size of bins
-        double avail_K = 8; // number of bins
-        int I = 10; // number of Items
-        std::vector<int> s{4,5,6,7,8,9,10,11,12,13}; //size of items
+    pagmo::vector_double fitness(const pagmo::vector_double &x_i) const {
+        // variables
+        double num_bins_used = 0; //how many bins are used
+        std::vector<int> bin_used(num_bins, 0);
 
-//        variables
-        double min_K = 0; //how many bins are used
-        std::vector<int> y_j(avail_K,0);
-
-//        setting y corresponding to X
-        for (int i = 0; i < I; ++i) {
-            for (int j = 0; j < avail_K; ++j) {
-                y_j[j] += x_ij[i*avail_K + j];
-            }
-        }
-
-//         objective function calculation
-        for(auto& y: y_j){
-            min_K += y;
-        }
-
-//         make return vector and adding objective
-        pagmo::vector_double return_vector{min_K};
-
-//        adding equality constraints - every task i in I has to be scheduled once.
-        for (int i = 0; i< I; ++i) {
-            int temp_x_sum  = 0;
-            for (int j = 0; j < avail_K; ++j) {
-                temp_x_sum += x_ij[i*avail_K + j];
-            }
-            return_vector.emplace_back(temp_x_sum - 1);
-        }
-
-//         adding inequality constraints with respect to at least one bin
-        return_vector.emplace_back(1 - min_K);
-        return_vector.emplace_back(min_K - avail_K);
-
-//         adding inequalities with respect to size of bins
-        for (int j = 0; j < avail_K; ++j) {
-            int temp_bin_occupation = 0;
-            for (int i = 0; i < I; ++i) {
-                temp_bin_occupation += s[i] * x_ij[i*avail_K + j];
-            }
-            return_vector.emplace_back(temp_bin_occupation - (B * y_j[j]));
-        }
-
-        return return_vector;
-    }
-
-    std::pair<pagmo::vector_double, pagmo::vector_double> get_bounds() const
-    {
-        std::vector<double> lower(80, 0);
-        std::vector<double> upper(80, 1);
-        return {lower, upper};
-    }
-
-};
-
-struct bin_packing2 {
-    // Number of equality constraints.
-    pagmo::vector_double::size_type get_nec() const{
-        return 0;
-    }
-
-    // Number of inequality constraints.
-    pagmo::vector_double::size_type get_nic() const{
-        return 10;
-    }
-
-    pagmo::vector_double::size_type get_nix() const
-    {
-        return 8;
-    }
-
-    // Implementation of the objective function and all constraints
-    pagmo::vector_double fitness(const pagmo::vector_double &x_i) const{
-//        dynamic variables: x_i,j where i in I and j in K
-
-        //constants
-        int B = 100; //size of bins
-        int avail_K = 8; // number of bins
-        int I = 10; // number of Items
-        std::vector<int> s{4,5,6,7,8,9,10,11,12,13}; //size of items
-
-//        variables
-        double min_K = 0; //how many bins are used
-        std::vector<int> y_j(avail_K,0); // if a certain bin is used
-
-
-//        setting y corresponding to X
-        for (int i = 0; i < I; ++i) {
-            for (int j = 0; j < avail_K; ++j) {
-                if (x_i[i] - j < 0.00001){
-                    y_j[j] = 1;
+        // where x_i is the bin in which an item i is placed
+        // determine if a bin is used based on x_i
+        for (int i = 0; i < num_items; ++i) {
+            for (int j = 0; j < num_bins; ++j) {
+//                if (-0.00001 < x_i[i] - j  && x_i[i] - j < 0.00001){ // if error is small enough the item i is placed in bin j
+                if ( x_i[i] == j){ // if error is small enough the item i is placed in bin j
+                    bin_used[j] = 1;
                 }
             }
         }
 
-//         objective function calculation
-        for(auto& y: y_j){
-            min_K += y;
+        // Objective function calculation; i.e. how many bins are used? We want to minimize this!
+        for(auto& y: bin_used){
+            num_bins_used += y;
         }
 
-//         make return vector and adding objective
-        pagmo::vector_double return_vector{min_K};
+        // Make return vector and adding objective
+        pagmo::vector_double return_vector{num_bins_used};
 
-//         adding inequality constraints with respect to at least one bin
-        return_vector.emplace_back(1 - min_K);
-        return_vector.emplace_back(min_K - avail_K);
-
-//         adding inequalities with respect to size of bins
-        for (int j = 0; j < avail_K; ++j) {
+        // Adding inequalities to stick to the maximum size of the bin
+        for (int j = 0; j < num_bins; ++j) {
             int temp_bin_occupation = 0;
-            for (int i = 0; i < I; ++i) {
+            for (int i = 0; i < num_items; ++i) {
                 if (x_i[i] == j){
-                    temp_bin_occupation += s[i] ;
+                    temp_bin_occupation += items[i] ; // <= 0
                 }
             }
-            return_vector.emplace_back(temp_bin_occupation - B);
+            return_vector.emplace_back(temp_bin_occupation - size_bins);
         }
-
         return return_vector;
     }
 
-    std::pair<pagmo::vector_double, pagmo::vector_double> get_bounds() const
-    {
-        std::vector<double> lower(8, 0);
-        std::vector<double> upper(8, 7);
+    // The lower and upper bounds are the bins in which an item can be placed.
+    std::pair<pagmo::vector_double, pagmo::vector_double> get_bounds() const {
+        std::vector<double> lower(num_items, 0);
+        std::vector<double> upper(num_items, num_bins-1);
         return {lower, upper};
     }
-
 };
 
 
 int main(){
-    pagmo::problem prob{bin_packing2{}};
-    std::cout << prob <<std::endl;
 
+    int size_bins = 7;
+    int num_bins = 6;
+    std::vector<int> items{3,2,5,1,4,1,1,1,1};
+
+    // 1 - Define the topology of the archipelago
+    pagmo::problem prob{bin_packing{size_bins, num_bins, items}};
+    std::cout << prob << std::endl;
+
+    // 2 - Define the topology of the archipelago
     pagmo::algorithm algo{pagmo::ihs(100)};
 
     // 3 - Define the topology of the archipelago
     pagmo::fully_connected topo{};
 
     // 4 - Instantiate an archipelago with 16 islands having each 100 individuals.
-    pagmo::archipelago archi{topo,8, algo, prob, 1000};
+    pagmo::archipelago archi{topo, 16, algo, prob, 100};
 
-    // 5 - Run the evolution in parallel on the 16 separate islands 10 times.
+    // 5 - Run the evolution in parallel on the 8 separate islands 10 times.
     archi.evolve(100);
 
     // 6 - Wait for the evolutions to finish.
     archi.wait_check();
 
-    // 7 - Print the fitness of the best solution in each island.
+    // 7 - Get the best solution across all islands. The first number in the champion vector is the number of islands used.
+    pagmo::population champion;
     for (const auto &isl : archi) {
-        std::cout << isl.get_population().champion_f()[0] << '\n';
+        if (champion.size() == 0){
+            champion = isl.get_population();
+        }
+        if (champion.champion_f()[0] > isl.get_population().champion_f()[0]){
+            champion = isl.get_population();
+        }
+    }
+
+    // 8 - Print the solution
+//    std::cout << champion.champion_f().size() << " " << champion.champion_x().size() << std::endl;
+    for (int i=0; i < champion.champion_x().size(); i++){
+        std::cout << "Item " << i << " is mapped to: " << champion.champion_x()[i] << std::endl;
+    }
+    // 9 - Print fitness value
+    std::cout << "Number of Islands used: " <<   champion.champion_f()[0] << std::endl;
+
+    for (int i=1; i < champion.champion_f().size(); i++){
+        std::cout << "Space left in bin " << i << " : " << -champion.champion_f()[i] << std::endl;
     }
 }
