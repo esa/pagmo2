8dc29cc974879f11c46ef1768a61b51505c3f824
Francesco Biscani <bluescarni@gmail.com>
2021-08-02 14:02:47 +0200

    Script fixes.

--- tools/circleci_ubuntu_arm64.sh
+++ tools/circleci_ubuntu_arm64.sh
@@ -14,8 +14,6 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda config --add channels conda-forge
-conda config --set channel_priority strict
 conda create -y -q -p $deps_dir c-compiler cxx-compiler cmake eigen nlopt boost-cpp tbb tbb-devel
 source activate $deps_dir
 
@@ -30,7 +28,7 @@ make -j2 VERBOSE=1
 # gives spurious warnings in the address sanitizer.
 # Also, enable the custom suppression file for ASAN
 # in order to suppress spurious warnings from TBB code.
-LSAN_OPTIONS=suppressions=/home/circleci/project/tools/lsan.supp ctest -j4 -V -E fork_island
+LSAN_OPTIONS=suppressions=/home/travis/build/esa/pagmo2/tools/lsan.supp ctest -j4 -V -E fork_island
 
 set +e
 set +x
