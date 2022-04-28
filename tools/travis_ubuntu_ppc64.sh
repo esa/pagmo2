06865aad249c1efd2081d998340b562c37b5ce01
Francesco Biscani <bluescarni@gmail.com>
2022-04-28 12:26:20 +0200

    Enable ipopt also on the arm64/ppc64 builds.

--- tools/travis_ubuntu_ppc64.sh
+++ tools/travis_ubuntu_ppc64.sh
@@ -14,7 +14,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda create -y -q -p $deps_dir c-compiler cxx-compiler cmake eigen nlopt boost-cpp tbb tbb-devel
+conda create -y -q -p $deps_dir c-compiler cxx-compiler cmake eigen nlopt ipopt boost-cpp tbb tbb-devel
 source activate $deps_dir
 
 # Create the build dir and cd into it.
@@ -22,7 +22,7 @@ mkdir build
 cd build
 
 # GCC build.
-cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DPAGMO_WITH_EIGEN3=yes -DPAGMO_WITH_NLOPT=yes
+cmake ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPAGMO_BUILD_TESTS=yes -DPAGMO_WITH_EIGEN3=yes -DPAGMO_WITH_NLOPT=yes -DPAGMO_WITH_IPOPT=yes
 make -j2 VERBOSE=1
 # Run the tests, except the fork island which
 # gives spurious warnings in the address sanitizer.
